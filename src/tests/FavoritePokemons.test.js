0e041c443b156cba9b99dd7d3cea43450ad2817e
belaguarino <izabelaguarino@gmail.com>
2021-09-28 16:40:43 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderRouter from './renderRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  beforeEach(() => {
+    renderRouter(<FavoritePokemons />);
+  });
+  test('Teste se é exibido na tela a mensagem', () => {
+    expect(screen.getByText(/No favorite pokemon found/i)).toBeInTheDocument();
+  });
+
+  test('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    const pokeName = screen.getByTestId('pokemon-name');
+    expect(pokeName).toBeInTheDocument();
+  });
+});
