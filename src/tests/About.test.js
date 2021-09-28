b7bb08b276f0809dba184a2c7e66c1b56b68f489
João Reis <joaoreisjpk@gmail.com>
2021-09-28 20:04:48 -0300

    requisito 1 e parte do 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent, render } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa se o navBar funciona devidamente', () => {
+  test('Verifica o texto, e se o link Home funciona devidamente', () => {
+    renderWithRouter(<App />);
+
+    const getAbout = screen.getByText(/about/i);
+    fireEvent.click(getAbout);
+
+    const getHeading = screen.getByRole('heading', {
+      level: 2,
+      name: /About Pokédex/,
+    });
+
+    expect(getHeading).toBeInTheDocument();
+
+    const getHeadings = screen.getAllByRole('heading');
+    expect(getHeadings.length).toBe(3);
+  });
+});
