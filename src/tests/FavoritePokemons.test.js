b606ef270b74714410155d540226a84f4bd91afe
Kelner Roberto <kelner250@hotmail.com>
2021-09-29 16:15:30 -0300

    requisito 3 - testando se renderiza na tela a mensagem quando nao ha pokemons favoritos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './Helpers/RenderRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('A Página de pokémons favoritos:', () => {
+  it('deve ser renderizada vazia quando não há nenhum pokémon favorito', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const titleWithNoFavorites = screen.getByText('No favorite pokemon found');
+    expect(titleWithNoFavorites).toBeInTheDocument();
+  });
+});
