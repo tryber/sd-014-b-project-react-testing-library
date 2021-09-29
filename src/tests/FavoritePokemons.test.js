b5ae34dceaeb4285683a2d82e2d2c12aba08da19
Michael <michaelcaxias.dev@gmail.com>
2021-09-28 21:49:54 -0300

    Req 3: Parte 1 completa

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+
+describe('3 - Testa o componente FavoritePokemons.js', () => {
+  it('Verifica mensagem de não encontrado caso não haja pokemóns favoritos', () => {
+    render(<FavoritePokemons />);
+    const notFound = screen.getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+  it('Verifica a exibição de pokemóns caso tenham sido favoritados', () => {
+    render(<FavoritePokemons />);
+  });
+});
