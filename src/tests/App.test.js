3b5ce68066a6135fdb464ef519245d6cc612668c
Israel <israel.fab@hotmail.com>
2021-09-29 10:17:40 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa se o topo da aplicação contém links de navegação', () => {
+  test('Se possui os links com os textos Home, About e Favorite Pokémons', () => {
+    renderWithRouter(<App />);
+
+    const home = screen.getByRole('link', { name: 'Home' });
+    expect(home).toBeInTheDocument();
+
+    const about = screen.getByRole('link', { name: 'About' });
+    expect(about).toBeInTheDocument();
+
+    const favorites = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(favorites).toBeInTheDocument();
+  });
+});
