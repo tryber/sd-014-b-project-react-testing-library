c81ac7cd72be2d621a958ddd38066ef2d4e3a335
Thyara <thyaranonato@outlook.com>
2021-09-29 15:46:31 -0300

    Impl: requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+describe('Testa o componente About', () => {
+  test('se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const aboutPokedex = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+
+  test('se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const twoParagraphs = screen.getAllByText(/Pokémons/);
+    expect(twoParagraphs).toHaveLength(2);
+  });
+
+  test('se a página contém a imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const pokedexImage = screen.getByRole('img');
+    expect(pokedexImage).toBeInTheDocument();
+    expect(pokedexImage).toHaveAttribute('src',
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(pokedexImage).toHaveAttribute('alt', 'Pokédex');
+  });
+});
