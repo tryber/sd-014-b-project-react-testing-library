01e5a806bb143df96138d782c0159b9262c6e930
Nataniel Santos <nathan.santos159@hotmail.com>
2021-09-29 19:15:57 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente NotFound', () => {
+  test('a pagina deve conter un h2 com o text Page requested not found 😭', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/notfound');
+    const h2 = screen.getByRole('heading', {
+      level: 2, name: 'Page requested not found Crying emoji' });
+    expect(h2).toBeInTheDocument();
+  });
+
+  test('deve conter um gif especifico', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/notfound');
+    const altText = 'Pikachu crying because the page requested was not found';
+    const gif = screen.getByAltText(altText);
+    expect(gif).toBeInTheDocument();
+  });
+});
