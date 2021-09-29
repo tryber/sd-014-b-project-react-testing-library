e9c4edd5839bace705bc7b174f6764ae4a5e8626
jarana-2020 <julioaranabr@yahoo.com.br>
2021-09-29 19:05:33 -0300

    Requirement 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import { NotFound } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente NotFound', () => {
+  test('se é exibido na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<NotFound />);
+    const textNotFound = screen.getByText(/Page requested not found/i);
+    expect(textNotFound).toBeInTheDocument();
+  });
+
+  test(' se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
