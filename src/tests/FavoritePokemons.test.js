cd6300e1a28adbcbdbdde74456cb700ac70fed66
Lucas Alves <lucalves.rodrigues@gmail.com>
2021-09-29 18:09:09 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { createMemoryHistory } from 'history';
 import { render, screen } from '@testing-library/react';
 import { Router } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import { FavoritePokemons } from '../components';
-/* import App from '../App'; */
+import App from '../App';
 
 describe('Testa o componente favoritePokemons', () => {
   test('Teste se sem favoritos aparece a mensagem No favorite pokemon found', () => {
@@ -22,12 +23,24 @@ describe('Testa o componente favoritePokemons', () => {
     const history = createMemoryHistory();
     render(
       <Router history={ history }>
-        <FavoritePokemons />
-        ,
+        <App />
       </Router>,
     );
-    const favButton = screen.getByAltText(/is marked as favorite/);
+    // procurou o botão de mais detalhes
+    const details = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(details);
 
-    expect(favButton).toHaveAttribute('src');
+    // depois que abriu o botão verifica o checkbox
+    const checkBox = screen.getByLabelText('Pokémon favoritado?');
+
+    // cria um evento de click para clicar no checkbox
+    userEvent.click(checkBox);
+    // clica no link para página de favoritos
+    const favPage = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favPage);
+    // confere se na página existe um pokemon favoritado através do test-id
+    const favCheckName = screen.getByTestId('pokemon-name');
+
+    expect(favCheckName).toBeInTheDocument();
   });
 });
