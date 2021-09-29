154dceb406e16f5d7111e91e4b1523c382d88701
Juliana Braga <juliana4braga@gmail.com>
2021-09-29 17:45:45 -0300

    Tenta 5 de novo

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa o card do Pokemon', () => {
+  test('se o nome do pokemon é mostrado', () => {
+    renderWithRouter(<App />);
+    const pokemonName = screen.getByTestId('pokemon-name');
+    expect(pokemonName.innerHTML).toBe('Pikachu');
+  });
+
+  test('se o peso é mostrado corretamente', () => {
+    renderWithRouter(<App />);
+    const pokemonWeight = screen.getByTestId('pokemon-weight');
+    expect(pokemonWeight.innerHTML).toBe('Average weight: 6.0 kg');
+  });
+
+  tes
+});
