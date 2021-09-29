a75107dcdc1550b022765cffd89eebd5596ffae2
Mathluiz23 <matheus_luizs@hotmail.com>
2021-09-29 14:47:43 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
+import App from '../App';
+
+describe('Testa os links do App e sua rotas', () => {
+  test('Primeiro link deve possuir o texto home', () => {
+    renderWithRouter(<App />);
+    const elementHome = screen.getByText(/home/i);
+    expect(elementHome).toBeInTheDocument();
+  });
+
+  test('Segundo link deve possuir o texto about', () => {
+    renderWithRouter(<App />);
+    const elementAbout = screen.getByText(/about/i);
+    expect(elementAbout).toBeInTheDocument();
+  });
+
+  test('Terceiro link deve possuir o texto Favorite Pokémons', () => {
+    renderWithRouter(<App />);
+    const elementFavorite = screen.getByText(/favorite pokémons/i);
+    expect(elementFavorite).toBeInTheDocument();
+  });
+});
