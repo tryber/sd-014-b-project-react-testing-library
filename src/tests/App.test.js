700117e920db062ce3fcb60a6ad94c6a99902280
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-09-29 17:10:12 -0300

    Atende requisito #2

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
 import App from '../App';
-
 import renderWithRouter from './renderWithRouter ';
 
 describe('Testando o componente "App.js"', () => {
