eb1c681b8f1702dcf4617a40264300bec35902c1
Nataniel Santos <nathan.santos159@hotmail.com>
2021-09-29 19:00:56 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import App from '../App';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente FavoritePokemons', () => {
+  test(
+    'deve conter a mensagem No favorite pokemon found, se não tiver pokémons favoritos.',
+    () => {
+      renderWithRouter(<FavoritePokemons />);
+
+      const message = screen.getByText('No favorite pokemon found');
+      expect(message).toBeInTheDocument();
+    },
+  );
+
+  test('deve exibir pokemons favoritados', () => {
+    renderWithRouter(<App />);
+
+    const moreDetails = screen.getByText('More details');
+    fireEvent.click(moreDetails);
+
+    const favoriteBtn = screen.getByLabelText('Pokémon favoritado?');
+    fireEvent.click(favoriteBtn);
+
+    const homeBtn = screen.getByRole('link', { name: 'Home' });
+    fireEvent.click(homeBtn);
+
+    const favoritePokemonsBtn = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    fireEvent.click(favoritePokemonsBtn);
+
+    const pokemonName = screen.getByText('Pikachu');
+    expect(pokemonName).toBeInTheDocument();
+  });
+});
