95c270954f12ed511eff4349cfec5765d4b19a05
Gustavo Sant'Anna <g_santanna@outlook.com>
2021-09-29 18:40:38 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderRoute from '../helper/renderRoute';
+
+describe('4 - Exiba uma página padrão para caminhos inexistentes', () => {
+  const imgURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+  const pathRoute = '/undefined';
+
+  test('Se ao navegar para um caminho inexistente exibe a mensagem padrão', () => {
+    const { history } = renderRoute(pathRoute);
+
+    expect(screen.getByRole('heading', {
+      level: 2,
+      name: /page requested not found/i,
+    })).toBeInTheDocument();
+
+    expect(history.location.pathname).toBe(pathRoute);
+  });
+
+  test('Se ao navegar para um caminho inexistente exibe a imagem padrão', () => {
+    const { history } = renderRoute(pathRoute);
+
+    const expectedImg = screen.getAllByRole('img').find((img) => img.src === imgURL);
+    expect(expectedImg).toBeInTheDocument();
+
+    expect(history.location.pathname).toBe(pathRoute);
+  });
+});
