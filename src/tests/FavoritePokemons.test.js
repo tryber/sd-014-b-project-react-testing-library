fcbc163a79bcddac311db9b14d2efef80d792c50
Ivanielson Cabral <ivanielson2011@gmail.com>
2021-09-29 18:59:18 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test(`Teste se é exibido na tela a mensagem No favorite pokemon found,
+    se a pessoa não tiver pokémons favoritos`, () => {
+    renderWithRouter(<FavoritePokemons />);
+    const pokemonFavoriteTitle = screen.getByRole('heading', {
+      level: 2,
+      name: 'Favorite pokémons',
+    });
+    expect(pokemonFavoriteTitle).toBeInTheDocument();
+
+    const notPokemonFavorite = screen.getByText('No favorite pokemon found');
+    expect(notPokemonFavorite).toBeInTheDocument();
+  });
+});
