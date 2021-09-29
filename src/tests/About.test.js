d8794dad654afb88ec1b8489a1151dbbe99edc0f
Emmanuel <EmmanuelRodriguesBoo@gmail.com>
2021-09-29 20:40:43 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from '../renderWithRouter';
+
+test('Testa se a página contém informações sobre a Pokedex', () => {
+  renderWithRouter(<About />);
+  const pokedex = screen.getByText('About Pokédex');
+  expect(pokedex).toBeInTheDocument();
+});
+
+test('Testa se a página contém um h2 com o texto "About Pokémon"', () => {
+  renderWithRouter(<About />);
+  const tag = screen.getByRole('heading', { name: 'About Pokédex' });
+  expect(tag).toBeInTheDocument();
+});
+
+test('Testa se a página contém dois parágrafos sobre a Pokédex', () => {
+  renderWithRouter(<About />);
+  const paragraph = screen.getAllByRole('p');
+  expect(paragraph.length).toBe(2);
+});
+
+test('Testa se a página contém a imagem de uma pokédex', () => {
+  renderWithRouter(<About />);
+  const img = screen.getByRole('img');
+  expect(img).toHaveAttribute(
+    'src',
+    'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+  );
+});
