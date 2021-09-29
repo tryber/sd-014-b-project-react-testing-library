85691deb0a11ade4b6888ef93bac9a860f7274b0
Lucas Alves <lucalves.rodrigues@gmail.com>
2021-09-29 14:31:59 -0300

    Requisitos 1 e 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,66 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { About } from '../components';
+
+describe('Testa o componente About', () => {
+  test('Teste se a página contém as informações sobre a Pokédex.', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <About />
+      </Router>,
+    );
+    const text = screen.queryByText(
+      /This application simulates a Pokédex/,
+    );
+
+    expect(text).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém o texto About Pokédex.', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <About />
+      </Router>,
+    );
+    const text = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+
+    expect(text).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém dois parágrafos.', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <About />
+      </Router>,
+    );
+    const text = screen.queryByText(
+      /This application simulates a Pokédex/,
+      /One can filter Pokémons by type/,
+    );
+      /* 'One can filter Pokémons by type, and see more details for each one of them', */
+
+    expect(text).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém uma imagem específica.', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <About />
+      </Router>,
+    );
+    const text = screen.queryByRole('img', {
+      src: 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    });
+
+    expect(text).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,66 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+describe('Teste se o topo da aplicaçãocontém um conjunto fixo de links de navegação',
+  () => {
+    test('O primeiro link deve possuir o texto Home', () => {
+      const history = createMemoryHistory();
+      render(
+        <Router history={ history }>
+          <App />
+        </Router>,
+      );
+
+      const link = screen.getByRole('link', { name: 'Home' });
+      userEvent.click(link);
+
+      const title = screen.getByRole('heading', {
+        level: 2,
+        name: 'Encountered pokémons',
+      });
+
+      expect(title).toBeInTheDocument();
+    });
+
+    test('O primeiro link deve possuir o texto About', () => {
+      const history = createMemoryHistory();
+      render(
+        <Router history={ history }>
+          <App />
+        </Router>,
+      );
+
+      const link = screen.getByRole('link', { name: 'About' });
+      userEvent.click(link);
+
+      const title = screen.getByRole('heading', {
+        level: 2,
+        name: 'About Pokédex',
+      });
+
+      expect(title).toBeInTheDocument();
+    });
+
+    test('O primeiro link deve possuir o texto Favorite pokémons', () => {
+      const history = createMemoryHistory();
+      render(
+        <Router history={ history }>
+          <App />
+        </Router>,
+      );
+
+      const link = screen.getByRole('link', { name: 'Favorite Pokémons' });
+      userEvent.click(link);
+
+      const title = screen.getByRole('heading', {
+        level: 2,
+        name: 'Favorite pokémons',
+      });
+
+      expect(title).toBeInTheDocument();
+    });
+  });
