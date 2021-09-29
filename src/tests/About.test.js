07c4c5fecf1566086f2dfb256266a01c45ab219c
luizsmatos <gusstavopdm@gmail.com>
2021-09-29 13:11:22 -0300

    Requisito 2 - Refatorado

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,9 +3,9 @@ import { screen } from '@testing-library/react';
 import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
 
-describe('2 - Quando a navegação para o About ocorre', () => {
+describe('2 - Teste se a página contém as informações sobre a Pokédex', () => {
 
-  it('Será validado se contém as informaçoes sobre a Pokédex', () => {
+  it('se a página contém um heading h2 com o texto About Pokédex', () => {
     const { history } = renderWithRouter(<App />);
     history.push('/about')
 
@@ -15,6 +15,15 @@ describe('2 - Quando a navegação para o About ocorre', () => {
     const title = screen.getByRole('heading', {level: 2, name: 'About Pokédex'});
     expect(title).toBeInTheDocument();
 
+  });
+
+  it('se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/about')
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+
     const firstParagraph = screen.getByText('This application simulates',
      { exact: false })
     expect(firstParagraph).toBeInTheDocument();
@@ -22,6 +31,15 @@ describe('2 - Quando a navegação para o About ocorre', () => {
     const secondParagraph = screen.getByText('One can filter Pokémons',
      { exact: false })
     expect(secondParagraph).toBeInTheDocument();
+
+  });
+
+  it('se a página contém a seguinte imagem de uma Pokédex', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/about')
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
 
     const imgPokedex = screen.getByRole('img');
     const urlImg = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
