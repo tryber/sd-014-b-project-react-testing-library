8d5278b91787a38636aa8b239ea8672e70fc2fde
perchespierri <perchespierri@gmail.com>
2021-09-29 13:53:12 -0300

    Requisitos 1 e 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './RenderWithRouter';
+import { About } from '../components';
+
+describe('Requisito 2 - Testa <About.js />', () => {
+  test('A página tem informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const pokedexInfo = screen.getByText(/This application simulates/i);
+    expect(pokedexInfo).toBeInTheDocument();
+  });
+
+  test('A página tem a heading h2 com o texto: About Pokédex', () => {
+    renderWithRouter(<About />);
+    const heading = screen.getByRole(
+      'heading', {
+        level: 2,
+        name: 'About Pokédex',
+      },
+    );
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('A página tem 2 parágrafos sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const paragraph1 = screen.getByText(/This/i);
+    const paragraph2 = screen.getByText(/One/i);
+    expect(paragraph1).toBeInTheDocument();
+    expect(paragraph2).toBeInTheDocument();
+  });
+
+  test('A página exibe a imagem de uma pokédex', () => {
+    renderWithRouter(<About />);
+    const imgTag = screen.getByRole('img');
+    expect(imgTag).toHaveAttribute(
+      'src',
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png',
+    );
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import event from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './RenderWithRouter';
+
+describe('Requisito 1 - Testa <App.js />', () => {
+  it('Verifica se Home, About e Favorite Pokémons estão na tela', () => {
+    renderWithRouter(<App />);
+    expect(screen.getByText('About')).toBeInTheDocument();
+    expect(screen.getByText('Home')).toBeInTheDocument();
+    expect(screen.getByText('Favorite Pokémons')).toBeInTheDocument();
+  });
+
+  // history grava a última location
+  // Muda com o próximo click
+  // Se desestruturar pathname de location e de history em um
+  // component, o próximo vai voltar o pathname do anterior
+
+  it('Verifica se home direciona para /', () => {
+    const { history } = renderWithRouter(<App />);
+    event.click(screen.getByText(/home/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Verifica se about direciona para /about', () => {
+    const { history } = renderWithRouter(<App />);
+    event.click(screen.getByText(/about/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  it('Verifica se favorite pokémons direciona para /favorites', () => {
+    const { history } = renderWithRouter(<App />);
+    event.click(screen.getByText(/favorite pokémons/i));
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+});
