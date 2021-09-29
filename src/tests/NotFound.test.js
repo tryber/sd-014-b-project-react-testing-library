d3dfea909b31cf8e4166a3042f1f9102c86c9ad8
Fernando Serpa <serpaef@outlook.com>
2021-09-29 18:55:59 -0300

    implementados testes do quarto requisito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('<NotFound.js />', () => {
+  test('página contém um heading h2 com o texto Page requested not found', () => {
+    render(<NotFound />);
+
+    const heading = screen.getByRole('heading', { level: 2 });
+
+    expect(heading).toHaveTextContent('Page requested not found');
+  });
+  test('Teste se página mostra a imagem', () => {
+    render(<NotFound />);
+
+    const image = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
