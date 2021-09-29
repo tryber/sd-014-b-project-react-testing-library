43b3a3cd075d267f08d6c40c1532f2dfe724545a
ronaldosenajr <ronaldosenajr@gmail.com>
2021-09-29 15:46:35 -0400

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o Componente "NotFound"', () => {
+  test(`se página contém um heading "h2" com o texto 
+  "Page requested not found 😭"`, () => {
+    renderWithRouter(<NotFound />);
+    const h2 = screen.getByRole('heading', { name: /Page requested not found/i });
+    expect(h2).toBeInTheDocument();
+  });
+
+  test(`se página mostra a imagem 
+  "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"`, () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getAllByRole('img');
+    expect(img[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
