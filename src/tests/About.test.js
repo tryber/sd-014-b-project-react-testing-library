feacf4676273e2ce6c400eff0e5fd0196173cca3
Danilo <danilo.santossouza@outlook.com>
2021-09-29 17:46:33 -0300

    Requisito 2 resolvido

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -21,10 +21,11 @@ describe('Testa a aplicação do component About', () => {
     expect(paragraphTwo).toBeInTheDocument();
   });
 
-  test('se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+  test('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
     render(<About />);
 
     const image = screen.getByRole('img');
     expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(image).toHaveAttribute('alt', 'Pokédex');
   });
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+// import userEvent from '@testing-library/user-event';
+// import { screen } from '@testing-library/react';
+// import React from 'react';
+// import renderWithRouter from './renderWithRouter';
+// import App from '../App';
+
+describe('Testa a aplicação do component FavoritePokemons', () => {
+  test('', () => {
+
+  });
+});
