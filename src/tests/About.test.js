a2a6df29b8a850f809e46cd7f63e0fa736e35eeb
Rafael Moraes - Turma 14 - Tribo B <rafael1moraes1rocha@gmail.com>
2021-09-29 17:05:57 -0300

    Projeto React Testing Library - Requisito 3

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -27,18 +27,6 @@ describe('Teste o componente <About.js />', () => {
 
     expect(title).toBeInTheDocument();
   });
-  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
-    const { history } = renderWithRouter(<App />);
-    history.push('/about');
-    const firstParaPhrase1 = 'This application simulates a Pokédex,';
-    const firstParaPhrase2 = ' a digital encyclopedia containing all Pokémons';
-    const firstParagraph = screen.getByText(firstParaPhrase1 + firstParaPhrase2);
-    const secParaPhrse1 = 'One can filter Pokémons by type,';
-    const secParaPhrse2 = ' and see more details for each one of them';
-    const secondParagraph = screen.getByText(secParaPhrse1 + secParaPhrse2);
-    expect(firstParagraph).toBeInTheDocument();
-    expect(secondParagraph).toBeInTheDocument();
-  });
   test(`Teste se a página contém a seguinte imagem de uma Pokédex: 
   https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png`,
   () => {
@@ -47,6 +35,6 @@ describe('Teste o componente <About.js />', () => {
 
     const image = screen.getByAltText('Pokédex');
     expect(image).toBeInTheDocument();
-    expect(image).toHaveAttribute('src', 'https://cdn2q.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png')
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,3 +1,45 @@
-describe('', () => {
-  test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  const favoritesUrl = '/favorites';
+  test(`Teste se é exibido na tela a mensagem No favorite pokemon found,
+   se a pessoa não tiver pokémons favoritos.`,
+  () => {
+    const { history } = renderWithRouter(<App />);
+    history.push(favoritesUrl);
+
+    const titleFavorites = screen.getByText('No favorite pokemon found');
+    expect(titleFavorites).toBeInTheDocument();
+
+    history.push('/');
+    const titleHome = screen.getByText('Encountered pokémons');
+    expect(titleHome).toBeInTheDocument();
+  });
+  test('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push(favoritesUrl);
+    const titleFavorites = screen.getByText('No favorite pokemon found');
+    expect(titleFavorites).toBeInTheDocument();
+
+    history.push('/');
+    const titleHome = screen.getByText('Encountered pokémons');
+    expect(titleHome).toBeInTheDocument();
+    const buttonFavorites = screen.getByRole('button', { name: 'Próximo pokémon' });
+    expect(buttonFavorites).toBeInTheDocument();
+    const moreDetails = screen.getByRole('link', { name: 'More details' });
+    expect(moreDetails).toBeInTheDocument();
+    userEvent.click(moreDetails);
+    const checkboxFavorite = screen.getByRole('checkbox',
+      { name: 'Pokémon favoritado?' });
+    expect(checkboxFavorite).toBeInTheDocument();
+    userEvent.click(checkboxFavorite);
+
+    history.push(favoritesUrl);
+    const favoriteText = screen.getByText('Pikachu');
+    expect(favoriteText).toBeInTheDocument();
+  });
 });
