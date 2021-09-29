1ac794428d609bc5c61675cb8f5bdb00e141a4d7
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-09-29 22:20:02 +0100

    Req.2(1/4): Testa informações sobre a Pokédex.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,20 @@
-test('Testa página `About`', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+describe('Testa componente `About`', () => {
+  test('se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const pokedexFirstParagraph = screen.getByText(
+      (content) => content.startsWith('This application'),
+    );
+
+    const pokedexSecondParagraph = screen.getByText(
+      (content) => content.startsWith('One can filter'),
+    );
+    expect(pokedexFirstParagraph).toBeInTheDocument();
+    expect(pokedexSecondParagraph).toBeInTheDocument();
+  });
+});
