3d55fea7bf55c051f48a47f378e6aed2e2d153b8
LucasAccurcio <lucas.accurcio@gmail.com>
2021-09-29 17:16:40 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,3 +1,25 @@
-Describe('Inicio projeto', () => {
-  test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Verifica se contém um conjunto fixo de links de navegação', () => {
+
+  it('Should have link Home', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByText('Home');
+    expect(linkHome).toBeInTheDocument();
+  });
+
+  it('Should have link About', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByText('About');
+    expect(linkHome).toBeInTheDocument();
+  });
+
+  it('Should have link Favorite Pokémons', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByText('Favorite Pokémons');
+    expect(linkHome).toBeInTheDocument();
+  });
 });
