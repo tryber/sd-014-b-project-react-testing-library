ef0907e25299abeb32a71ae888c04b66a2ea0fd2
dineinunes <nunesvieiraclaudinei@outlook.com>
2021-09-29 18:54:38 -0300

    Soluciona requisito 1

---
+++ src/renderWithRouter.js
@@ -0,0 +1,15 @@
+// Essa função foi retirada da matéria disponibilizada no course
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return ({
+    ...render(<Router history={history}>{component}</Router>), history,
+  });
+};
+
+export default renderWithRouter;
