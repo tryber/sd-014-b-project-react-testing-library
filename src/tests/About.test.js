266e577a91966ecc2bbf9a9d2852fdc156d81d53
Rafael Veiga <rafaelveig@hotmail.com>
2021-09-29 18:01:24 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { About } from '../components';
+
+describe('Requisito 2', () => {
+  it('A página contém as informações sobre a Pokédex?', () => {
+    renderWithRouter(<About />);
+    const info = screen.getAllByText(/Pokémons/);
+    // console.log('oi', info);
+    expect(info).toHaveLength(2);
+    // testing-library.com/docs/react-testing-library/cheatsheet/
+  });
+  it('A página contém um heading h2 com o texto "About Pokédex"? ', () => {
+    renderWithRouter(<About />);
+    const h2 = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(h2).toBeInTheDocument();
+  });
+  it('A pagina contém a seguinte imagem?', () =>{
+    renderWithRouter(<About />);
+    const img = screen.getByRole('img');
+    const src = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(img.src).toBe(src);
+  })
+});
