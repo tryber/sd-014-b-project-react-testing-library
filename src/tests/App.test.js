6758dc88a909ce683e418dd2623f538dee97b52b
Gustavo <gus.inacio@hotmail.com>
2021-09-29 15:42:25 -0300

    Requirement #1 Testing App.js component.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+test('se a aplicação contem links de navegação fixos', () => {
+  const history = createMemoryHistory();
+  render(
+    <Router history={ history }>
+      <App />
+    </Router>,
+  );
+
+  const homeLink = screen.getByRole('link', { name: 'Home' });
+  expect(homeLink).toBeInTheDocument();
+
+  const aboutLink = screen.getByRole('link', { name: 'About' });
+  expect(aboutLink).toBeInTheDocument();
+
+  const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+  expect(favoriteLink).toBeInTheDocument();
+});
