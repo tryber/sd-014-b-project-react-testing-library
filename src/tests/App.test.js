2820e1dbd32249af629ffd8c69d59d2fff8984f9
Kelner Roberto <kelner250@hotmail.com>
2021-09-29 14:32:01 -0300

    commit inicial com requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('o componente app', () => {
+  it('deve possuir um cabeçalho fixo de navegação', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    expect(homeLink).toBeInTheDocument();
+
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    expect(aboutLink).toBeInTheDocument();
+
+    const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(favoriteLink).toBeInTheDocument();
+
+    userEvent.click(homeLink);
+    expect(history.location.pathname).toBe('/');
+
+    userEvent.click(aboutLink);
+    expect(history.location.pathname).toBe('/about');
+  });
+});
