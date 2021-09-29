39cf75fbe9835d6742ab7f2331e650a1be98f3b9
Rafael Veiga <rafaelveig@hotmail.com>
2021-09-29 15:55:50 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,41 @@
-test('rafael veiga ', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Requisito 1', () => {
+  test('Existe link de navegação?', () => {
+    renderWithRouter(<App />);
+    const links = screen.getAllByRole('link');
+    expect(links[0]).toHaveTextContent('Home');
+    expect(links[1]).toHaveTextContent('About');
+    expect(links[2]).toHaveTextContent('Favorite Pokémons');
+    // https://github.com/testing-library/jest-dom#tohavetextcontent
+  });
+  it('Ao clicar em home redireciona para pagina inicial?', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeBTN = screen.getByText('Home');
+    userEvent.click(homeBTN);
+    expect(history.location.pathname).toBe('/');
+  });
+  it('Ao clicar em about redireciona para about?', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutBTN = screen.getByText('About');
+    userEvent.click(aboutBTN);
+    expect(history.location.pathname).toBe('/about');
+  });
+  it('Ao clicar em Pokémons Favoritos redireciona para a página de favoritos?', () => {
+    const { history } = renderWithRouter(<App />);
+    const favBTN = screen.getByText('Favorite Pokémons');
+    userEvent.click(favBTN);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+  it('Renderiza "Not Found" em caso de url desconhecida?', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('pagina-desconhecida');
+    const notFound = screen.getByText(/not found/);
+    expect(notFound).toBeInTheDocument();
+  });
+  // regex passou pq?
+});
