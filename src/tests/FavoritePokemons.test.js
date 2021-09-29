8d73fe78d9b17cc2f3c83cb52236d6dc171bc8cd
jarana-2020 <julioaranabr@yahoo.com.br>
2021-09-29 18:14:03 -0300

    Requirement 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,37 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente Favoritepokemons', () => {
+  test('se é exibido na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const notFoundPokemon = screen.getByText(/No favorite pokemon found/i);
+    expect(notFoundPokemon).toBeInTheDocument();
+  });
+
+  test('se é exibido todos os cards de pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    const LinkDetails = screen.getByRole('link', { name: 'More details' });
+    expect(LinkDetails).toBeInTheDocument();
+    userEvent.click(LinkDetails);
+    const url = history.location.pathname;
+    expect(url).toBe('/pokemons/25');
+    const checkFavorite = screen.getByRole('checkbox');
+    expect(checkFavorite).not.toBeChecked();
+    userEvent.click(checkFavorite);
+    expect(checkFavorite).toBeChecked();
+    const linkFavorites = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(linkFavorites);
+    const urlFavorites = history.location.pathname;
+    expect(urlFavorites).toBe('/favorites');
+    const namePokemon = screen.getByTestId('pokemon-name');
+    const typePokemon = screen.getByTestId('pokemon-type');
+    expect(namePokemon).toBeInTheDocument();
+    expect(namePokemon).toHaveTextContent('Pikachu');
+    expect(typePokemon).toBeInTheDocument();
+    expect(typePokemon).toHaveTextContent('Electric');
+  });
+});
