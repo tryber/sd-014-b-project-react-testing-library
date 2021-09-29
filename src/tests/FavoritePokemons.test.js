5ef142c2061c4d503c971ad8588a611885e9b2a8
Leomar Linhares <leomar_linhares@hotmail.com>
2021-09-29 18:56:57 -0300

    cumpre o requisito 3 :rocket:

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente About', () => {
+  it('deveria apresentar o texto `About Pokédex` no título', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const pokemonsNotFound = screen.getByText('No favorite pokemon found');
+    expect(pokemonsNotFound).toBeInTheDocument();
+  });
+});
