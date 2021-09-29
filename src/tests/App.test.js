0af91259d9a18519a7fe4913785599eb387ee2d7
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-09-29 16:39:15 -0300

    Atende requisito #1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+import renderWithRouter from './renderWithRouter ';
+
+describe('Testando o componente "App.js"', () => {
+  test('Verificar se os links do componente "App.js" renderizam.', () => {
+    renderWithRouter(<App />);
+    const links = screen.getAllByRole('link');
+    expect(links[0]).toHaveTextContent('Home');
+    expect(links[1]).toHaveTextContent('About');
+    expect(links[2]).toHaveTextContent('Favorite Pokémons');
+  });
+});
