3c3cc246bc3a8185cee3b2b874b5da8ba15c65c5
FelipeMartins <mfelipeluiz99@gmail.com>
2021-09-29 18:02:56 -0300

    Requisito 1 - Lint Fix

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { screen, fireEvent } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './utils/renderWithRouter';
 
