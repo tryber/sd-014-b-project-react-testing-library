263d5327eabde10bc4307b9e7f102ea9c083d646
Henrique Almeida <joaoh.almeidaaraujo@gmail.com>
2021-09-29 16:08:01 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,2 +1,47 @@
-test('', () => {});
-// É biscoito.
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './services/renderWithRouter';
+
+describe('Testa o componente <App.js />', () => {
+  it('Deveria, no topo da aplicação, conter links de navegação', () => {
+    renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    const favoritesLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favoritesLink).toBeInTheDocument();
+  });
+
+  // it(`Deveria, ao clicar no botão "Home",
+  // redirecionar o usuário para a página inicial`, () => {
+  //   const { history } = renderWithRouter(<App />);
+  //   const homeLink = screen.getByRole('link', { name: 'Home' });
+
+  //   userEvent.click(homeLink);
+
+  //   expect(history.location.pathname).toBe('/');
+  // });
+
+  // it(`Deveria, ao clicar no botão "About",
+  // redirecionar o usuário para a página de detalhes`, () => {
+  //   const { history } = renderWithRouter(<App />);
+  //   const aboutLink = screen.getByRole('link', { name: 'About' });
+
+  //   userEvent.click(aboutLink);
+
+  //   expect(history.location.pathname).toBe('/about');
+  // });
+  // it(`Deveria, ao clicar no botão "Favorite Pokémons",
+  // redirecionar o usuário para a página de pokémons favoritos`, () => {
+  //   const { history } = renderWithRouter(<App />);
+  //   const favoritesLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+  //   userEvent.click(favoritesLink);
+
+  //   expect(history.location.pathname).toBe('/favorites');
+  // });
+});
