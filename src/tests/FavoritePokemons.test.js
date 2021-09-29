96aac6814f075f7943c905a80e9b4c7bdaa98710
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-09-29 17:26:14 -0300

    Atende requisito #3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter ';
+
+describe('Testando o componente "FavoritePokemons.js"', () => {
+  test('Verificar se o componente "FavoritePokemons.js" renderiza.', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+
+    const noFavoritePokemonText = screen.getByText(/No favorite/);
+    expect(noFavoritePokemonText).toBeInTheDocument();
+    expect(noFavoritePokemonText).toHaveTextContent('No favorite pokemon found');
+  });
+});
