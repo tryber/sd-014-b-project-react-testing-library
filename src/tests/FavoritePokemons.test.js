745c7fb66428c5585afb04c01208a3a56799b718
Diogo Fiuza <difipalm@gmail.com>
2021-09-29 17:48:33 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Requisito 3', () => {
+  test('Verifica se possui o texto caso não tenha conteúdo', () => {
+    render(<FavoritePokemons />);
+    const contentP = screen.getByText('No favorite pokemon found');
+    expect(contentP).toBeInTheDocument();
+  });
+
+  test('Verifica se mostra os cards dos pokemons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const buttonDetails = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(buttonDetails);
+
+    const checkboxFavorited = screen.getByRole('checkbox', { id: 'favorite' });
+    userEvent.click(checkboxFavorited);
+
+    history.push('/favorites');
+
+    const favoritedPokemon = screen.getByText(/Average weight:/i);
+    expect(favoritedPokemon).toBeInTheDocument();
+  });
+});
