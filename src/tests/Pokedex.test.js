a440ac460e5e0de8de44dfbd630b08a8a9c202ab
Michael <michaelcaxias.dev@gmail.com>
2021-09-28 22:15:00 -0300

    Req 5: Iniciado, mas não completo

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { Pokedex } from '../components';
+import pokemons from '../data';
+
+describe('5 - Testa o arquivo Pokedex.js', () => {
+  it('Verifica se outro pokémon é exibido quando `Proximo Pokemon` é clicado', () => {
+    render(<Pokedex pokemon={ pokemons } />);
+    const nextPokemon = screen.getByTestId('pokemon-type-button');
+    expect(nextPokemon).toBeInTheDocument();
+  });
+});
