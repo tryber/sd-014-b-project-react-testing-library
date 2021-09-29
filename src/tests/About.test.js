a7652ed310453c62d10ad6a51e63db600ad1a377
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-09-29 22:37:33 +0100

    Ajuste req2 [1/4 e 3/4]

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,9 +3,22 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import About from '../components/About';
+import App from '../App';
 
 describe('Testa componente `About`', () => {
   test('se a página contém as informações sobre a Pokédex', () => {
+    const { history } = renderWithRouter(<App />);
+    const aboutLink = screen.getByRole('link', {
+      name: 'About',
+    });
+
+    userEvent.click(aboutLink);
+
+    expect(aboutLink).toBeInTheDocument();
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  test('se a página contém dois parágrafos com texto sobre a Pokédex', () => {
     renderWithRouter(<About />);
     const pokedexFirstParagraph = screen.getByText(
       (content) => content.startsWith('This application'),
