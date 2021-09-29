aec4d3f1278f026007eab9cb80af6fe648618acf
jarana-2020 <julioaranabr@yahoo.com.br>
2021-09-29 19:06:26 -0300

    Reafactored Requirement 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import App from '../App';
 import { NotFound } from '../components';
 import renderWithRouter from '../renderWithRouter';
 
