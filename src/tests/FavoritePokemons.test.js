b8f07c646e3fcee229b4071a086b1e10037c6e73
Guilherme <guimfontes7@gmail.com>
2021-09-29 18:03:02 -0300

    Tests NotFound component

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { cleanup, screen, render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+afterEach(cleanup);
+
+describe('applies tests for favorite pokémons page', () => {
+  it('should display a not found text if none are marked', () => {
+    render(<FavoritePokemons />);
+    const notFound = screen.getByText(/No favorite pokemon found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+});
