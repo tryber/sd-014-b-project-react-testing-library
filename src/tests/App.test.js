5a2e2b6994a6a435e24b1ecf7a5d12fdfff9c106
Danilo <danilo.santossouza@outlook.com>
2021-09-29 15:31:36 -0300

    Requisito 1 resolvido

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testa a aplicação do component App', () => {
+  test('se a aplicação é redirecionada para a página inicial', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    userEvent.click(linkHome);
+    const pathName = history.location.pathname;
+    expect(pathName).toBe('/');
+  });
+
+  test('se a aplicação é redirecionada para a página de About', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: 'About' });
+    userEvent.click(linkHome);
+    const pathName = history.location.pathname;
+    expect(pathName).toBe('/about');
+  });
+
+  test('se a aplicação é redirecionada para a página de Pokémons Favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkHome = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(linkHome);
+    const pathName = history.location.pathname;
+    expect(pathName).toBe('/favorites');
+  });
+});
