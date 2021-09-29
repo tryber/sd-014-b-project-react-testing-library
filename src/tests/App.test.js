fd3d946008354ec9860699555285f4f565527add
Thyara <thyaranonato@outlook.com>
2021-09-29 15:20:09 -0300

    Impl: requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente App', () => {
+  test('se a aplicação é redirecionada para a página Inicial', () => {
+    const { history } = renderWithRouter(<App />);
+    userEvent.click(screen.getByRole('link', { name: 'Home' }));
+    expect(history.location.pathname).toBe('/');
+  });
+
+  test('se a aplicação é redirecionada para a página About', () => {
+    const { history } = renderWithRouter(<App />);
+    userEvent.click(screen.getByRole('link', { name: 'About' }));
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  test('se a aplicação é redirecionada para a página Pokémons Favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    userEvent.click(screen.getByRole('link', { name: 'Favorite Pokémons' }));
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  test('se a aplicação é redirecionada para a página Not Found', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pagina-que-não-existe');
+    expect(history.location.pathname).toBe('/pagina-que-não-existe');
+  });
+});
