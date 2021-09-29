4da235142cd006aa61fb8bbc2c4f17d9eb24ff60
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-09-29 16:02:27 -0300

    Testa componente FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import RouterRender from './RouterRender';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Testa o componente <FavoritePokemons.js />', () => {
+  test(`É exibido na tela a mensagem "No favorite pokemon found", 
+  se a pessoa não tiver pokémons favoritos`, () => {
+    RouterRender(<FavoritePokemons />);
+    const alert = screen.getByText(/No favorite pokemon found/);
+    expect(alert).toBeInTheDocument();
+  });
+
+  test('É exibido todos os cards de pokémons favoritados.', () => {
+    RouterRender(<App />);
+    const buttonDetails = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(buttonDetails);
+
+    const favoriteInput = screen.getByRole('checkbox');
+    userEvent.click(favoriteInput);
+
+    const favPokemonsLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favPokemonsLink);
+
+    const namePokemon = screen.getByTestId('pokemon-name');
+    expect(namePokemon).toBeInTheDocument();
+  });
+});
