dfa5f8dee6fd184e7875648e30610569ef72e0c3
Henrique Almeida <joaoh.almeidaaraujo@gmail.com>
2021-09-29 17:10:07 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './services/renderWithRouter';
+
+describe('Testa o componente "<FavoritePokemons.js />"', () => {
+  it(`Deveria, ao não haverem pokemons favoritados,
+  renderizar um parágrafo com o texto "No favorite pokemon found"`, () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const noPokemonParagraph = screen.getByText('No favorite pokemon found');
+
+    expect(noPokemonParagraph).toBeInTheDocument();
+  });
+});
