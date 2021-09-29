7a76a324560ba526ca4bc713b2284980e1889e89
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-09-29 17:55:59 -0300

    Atende requisito  #4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter ';
+
+describe('Testando o componente "NotFound.js', () => {
+  test('Verificar se o texto "Page requested not found" .', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemon-inexistente');
+
+    const notFoundText = screen.getByText(/Page requested not found/);
+    expect(notFoundText).toBeInTheDocument();
+  });
+
+  test('Verificar se a imagem do componente renderiza.', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemon-inexistente');
+
+    const notFoundImage = screen.getByAltText(/Pikachu crying because the page/);
+    expect(notFoundImage).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
