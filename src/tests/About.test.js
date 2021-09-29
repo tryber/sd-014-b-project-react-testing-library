66e0c45f37cf735edca4e174a4f490651da59d60
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-09-29 19:04:20 -0300

    Req 2.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('test component About', () => {
+  test('if the page contains a h2 with the text "About Pokédex"', () => {
+    render(<About />);
+    const title = screen.getByRole('heading', {
+      name: /About Pokédex/i,
+    });
+    expect(title).toBeInTheDocument();
+  });
+  test('if the page contains 2 paragraphs with text about Pokédex', () => {
+    render(<About />);
+    const firstParagraph = screen.getByText(/This application simulates/i);
+    const secondParagraph = screen.getByText(/One can filter/i);
+    expect(firstParagraph).toBeInTheDocument();
+    expect(secondParagraph).toBeInTheDocument();
+  });
+  test('if the page contains an image of a Pokédex', () => {
+    render(<About />);
+    const image = screen.getByRole('img');
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(image).toHaveAttribute('alt', 'Pokédex');
+    expect(image).toBeInTheDocument();
+  });
+});
