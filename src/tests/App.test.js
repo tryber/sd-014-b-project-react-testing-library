01a047100b7c0eb990cd5fe53d858cd9abbeb3b4
José Victor Macedo Veloso <vitovls37@gmail.com>
2021-09-29 15:41:33 -0300

    Req 01

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router';
+import { createBrowserHistory } from 'history';
+import App from '../App';
+
+const history = createBrowserHistory();
+
+describe('Testes do componente `<App.js />`', () => {
+  it('se o topo da aplicação contém um conjunto fixo de links de navegação.', () => {
+    render(<Router history={ history }><App /></Router>);
+
+    const linksNav = screen.getAllByRole('link');
+    const lengthMaxLinks = 4;
+
+    expect(linksNav).toHaveLength(lengthMaxLinks);
+    expect(linksNav[0]).toContainHTML('Home');
+    expect(linksNav[1]).toContainHTML('About');
+    expect(linksNav[2]).toContainHTML('Favorite Pokémons');
+  });
+});
