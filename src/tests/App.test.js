a3cb40cf509153299777fe0b1053d8e9c2b448ce
Gustavo Sant'Anna <g_santanna@outlook.com>
2021-09-29 15:58:31 -0300

    Primeiro requisito, início de projeto

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,44 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderRoute from '../helper/renderRoute';
+
+describe('1 - Crie links de navegação para as páginas', () => {
+  test('Se a página possui o link para "About"', () => {
+    const { history } = renderRoute('/');
+
+    userEvent.click(screen.getByRole('link', { name: 'About' }));
+
+    expect(screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    })).toBeInTheDocument();
+
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  test('Se a página possui o link para "Home"', () => {
+    const { history } = renderRoute('/about');
+
+    expect(history.location.pathname).toBe('/about');
+
+    userEvent.click(screen.getByRole('link', { name: 'Home' }));
+
+    expect(screen.getByRole('heading', {
+      level: 2,
+      name: 'Encountered pokémons',
+    })).toBeInTheDocument();
+  });
+
+  test('Se a página possui o link para "Favorite Pokémons"', () => {
+    const { history } = renderRoute('/');
+
+    userEvent.click(screen.getByRole('link', { name: 'Favorite Pokémons' }));
+
+    expect(screen.getByRole('heading', {
+      level: 2,
+      name: 'Favorite pokémons',
+    })).toBeInTheDocument();
+
+    expect(history.location.pathname).toBe('/favorites');
+  });
+});
