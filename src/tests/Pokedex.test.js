546f0f240e36fca7008c9ad878f2462dbca745bc
LucasAccurcio <lucas.accurcio@gmail.com>
2021-09-30 15:09:10 -0300

    Correcao LINT

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
-import { Pokedex } from '../components';
 import renderWithRouter from './renderWithRouter';
 
 describe('Testing component Pokedex', () => {
