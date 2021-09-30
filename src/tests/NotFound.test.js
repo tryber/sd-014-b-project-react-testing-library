80c50e38cb047266dd20d6fdcd1df5fc7e4132c2
José Victor Macedo Veloso <vitovls37@gmail.com>
2021-09-29 21:47:59 -0300

    Implementando testes de <NotFound.js /> - Requisito 4 :white_check_mark:

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testes do componente <NotFound.js />', () => {
+  it('Se página contém um h2 com o texto Page requested not found 😭;', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/notExist');
+
+    const title = screen.getByRole('heading', { level: 2,
+      name: /Page requested not found/i,
+      exact: false });
+    expect(title).toBeInTheDocument();
+  });
+  it('Se página mostra a imagem "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/notExist');
+
+    const imgPokedex = screen.getByRole('img', { name: /Pikachu crying/i });
+    expect(imgPokedex).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
