77f153d139395b01f5c76ed8f95c1173b4d42cdf
Leomar Linhares <leomar_linhares@hotmail.com>
2021-09-30 10:24:04 -0300

    cumpre a primeira parte do requisito 5 :rocket:

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Teste do componente Pokedex', () => {
+  it('deveria conter um <h2> com o texto `Encountered pokémons`', () => {
+    renderWithRouter(<App />);
+    const title = screen.getByRole('heading', {
+      name: 'Encountered pokémons',
+      level: 2,
+    });
+    expect(title).toBeInTheDocument();
+  });
+});
