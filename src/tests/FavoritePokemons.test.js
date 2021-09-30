84e405c35c1c6ef3faa7c4a16e5e5190f45ff616
Wallacy Francis <wallacy_francis@hotmail.com>
2021-09-30 15:38:15 -0300

    Requisito 3 resolvido - FavoritePokemons.test.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,49 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+test('Se é exibido a frase "No favorite pokemon found"', () => {
+  render(<FavoritePokemons />);
+  const empty = screen.getByText(/No favorite pokemon found/i);
+
+  expect(empty).toBeInTheDocument();
+});
+
+test('Se aparece o cards de pokemons favoritos', () => {
+  const pokemonCard = [
+    {
+      averageWeight: {
+        measurementUnit: 'kg',
+        value: '6.0',
+      },
+      foundAt: [
+        {
+          location: 'Kanto Viridian Forest',
+          map: 'https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png',
+        },
+        {
+          location: 'Kanto Power Plant',
+          map: 'https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png',
+        },
+      ],
+      id: 25,
+      image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+      moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Pikachu_(Pok%C3%A9mon)',
+      name: 'Pikachu',
+      // eslint-disable-next-line max-len
+      summary: 'This intelligent Pokémon roasts hard berries with electricity to make them tender enough to eat.',
+      type: 'Electric',
+    },
+  ];
+  const history = createMemoryHistory();
+  render(
+    <Router history={ history }>
+      <FavoritePokemons pokemons={ pokemonCard } />
+    </Router>,
+  );
+  const pokemonName = screen.getByText('Pikachu');
+
+  expect(pokemonName).toBeInTheDocument();
+});
