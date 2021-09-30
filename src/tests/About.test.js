2aeb71e28e2365258edc8e17a5a1401fce9bbffa
Bruno M Landim <brunolandim13@hotmail.com>
2021-09-30 19:36:23 -0300

    Requisito 2 e 4

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import About from '../components/About';
+
+describe('Teste se a página contém as informações sobre a Pokédex.', () => {
+  it('A página contém um heading h2 com o texto About Pokédex', () => {
+    render(<About />);
+
+    const aboutPokedex = screen.getByRole('heading', { level: 2, name: 'About Pokédex' });
+
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+
+  it('A página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    render(<About />);
+
+    const paragra1 = screen.getByText(/This application simulates a Pokédex/);
+    const paragra2 = screen.getByText(/One can filter Pokémons by type/);
+
+    expect(paragra1).toBeInTheDocument();
+    expect(paragra2).toBeInTheDocument();
+  });
+
+  it('A página contém a seguinte imagem de uma Pokédex:', () => {
+    render(<About />);
+
+    const image = screen.getByRole('img');
+
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Teste o componente FavoritePokemons', () => {
+  it('mensagem de "No favorite pokemon found", se não houver pokémons favoritos', () => {
+    render(<FavoritePokemons />);
+
+    const notFoudmensage = screen.getByText('No favorite pokemon found');
+    expect(notFoudmensage).toBeInTheDocument();
+  });
+});
