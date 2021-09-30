2866db7c7e5131fd39d28648d7465c063d0484a6
Mariana Ferreira <marianadesouzaferreira01@gmail.com>
2021-09-30 17:59:35 -0300

    Requisito 3 - FavoritePokemons.test.js

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+import renderWithRouter from './renderWithRouter';
+
+describe('', () => {
+  it('É exibido na tela No favorite pokemon found quando não há pokemon favorito', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFavoritePokemon = screen.getByText(/No favorite pokemon found/);
+    expect(noFavoritePokemon).toBeDefined();
+  });
+});
