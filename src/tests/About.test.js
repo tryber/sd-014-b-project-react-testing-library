33d5295f9d80dd24b5ad8c2e0374981c54165724
Vitor Lima <vitorliman@gmail.com>
2021-09-29 23:53:53 -0300

    Finaliza requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import About from '../components/About';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa as funcionalidades do About', () => {
+  it('testa se a página possui um h2', () => {
+    renderWithRouter(<About />);
+    const heading = screen.getByRole('heading', { name: 'About Pokédex' });
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('testa se a página possui 2 parágrafos', () => {
+    renderWithRouter(<About />);
+    const paragraphOne = screen.getByText(/This application simulates a Pokédex/i);
+    const paragraphTwo = screen.getByText(/One can filter Pokémons by type/i);
+
+    expect(paragraphOne).toBeInTheDocument();
+    expect(paragraphTwo).toBeInTheDocument();
+  });
+
+  it('testa se a pagina possui uma imagem', () => {
+    renderWithRouter(<About />);
+    const img = screen.getByRole('img');
+    expect(img).toHaveProperty('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
