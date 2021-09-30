cf7d5288c7c15c993d2ff664be1ddaa608433301
Elithon Silva <elithonsilva@gmail.com>
2021-09-30 15:54:05 -0300

    inicia teste requisito 1 e cria componente renderWithRouter

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+test('se o primeiro link possui o texto "Home"', () => {
+  render(<App />);
+  const linkText = screen.getByAltText('Home');
+  expect(linkText).toBeInTheDocument();
+});
