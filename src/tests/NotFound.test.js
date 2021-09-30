d2184eaaa04e7e3fc42a0e98161d3470cd04347e
lucas <mr.pine97@gmail.com>
2021-09-30 13:36:25 -0300

    impl: verifica o texto

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import RenderWithRouter from './RenderWithRouter';
+
+describe('testa o componente NotFound.js', () => {
+  test('se página contém um heading h2 com o texto `Page requested not found`', () => {
+    const { history } = RenderWithRouter(<App />);
+    history.push('/uma-pagina-inexistente');
+    const headingText = screen.getByRole('heading', {
+      level: 2,
+      name: 'Page requested not found Crying emoji',
+    });
+    expect(headingText).toBeInTheDocument();
+  });
+});
