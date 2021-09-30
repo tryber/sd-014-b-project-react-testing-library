ce3821bd7d42dd5e6b7a74bf0a757fadf457b46c
Max Salewski <maxsalewski@gmail.com>
2021-09-30 14:53:25 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente About.js', () => {
+  test('Testa se contém um heading h2 com o texto Page requested not found 😭', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/badrouter');
+    const notFound = screen.getByRole('heading',
+      { level: 2, name: 'Page requested not found Crying emoji' });
+    expect(notFound).toBeInTheDocument();
+  });
+  test('Testa se a página contém a imagem', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/badrouter');
+    const image = screen.getByAltText('Pikachu crying', { exact: false });
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
