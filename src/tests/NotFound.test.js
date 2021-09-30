63b56ca1ee73231f6231d98f7033fac93875dd7a
denisespanhol <denisgfespanhol@gmail.com>
2021-09-30 11:53:58 -0300

    Requisito 4 completo

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { NotFound } from '../components';
+
+describe('NotFound.js tests set', () => {
+  it('should the page Not Found having the suggested describe', () => {
+    // Teste se página contém um heading h2 com o texto Page requested not found.
+    render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+
+    const textNotFound = screen.getByRole('heading', {
+      level: 2, name: /page requested not found/i });
+    expect(textNotFound).toBeInTheDocument();
+  });
+
+  it('should the page Not Found shows the solicited image', () => {
+    // Teste se página mostra a imagem solicitada.
+    render(
+      <MemoryRouter>
+        <NotFound />
+      </MemoryRouter>,
+    );
+
+    const imgNotFound = screen.getByRole('img', { name: /pikachu crying/i });
+    expect(imgNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
