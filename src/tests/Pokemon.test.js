dc1ba2e08def9f87585ac270a27d7216e87f44e4
Gustavo Sant'Anna <g_santanna@outlook.com>
2021-09-30 02:03:02 -0300

    Requisito 6 upgrade

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -15,6 +15,9 @@ describe('6 - Renderize o card de um pokemon', () => {
 
     userEvent.click(screen.getByRole('link', { name: /details/i }));
 
+    expect(screen.getAllByRole('img')
+      .some((img) => img.alt === `${pokemons[0].name} is marked as favorite`))
+      .toBe(false);
     userEvent.click(screen.getByRole('checkbox', { checked: false }));
     const starIcon = screen.getByAltText(`${pokemons[0].name} is marked as favorite`);
     expect(starIcon).toBeInTheDocument();
@@ -34,6 +37,18 @@ describe('6 - Renderize o card de um pokemon', () => {
     expect(pokeImg1.src).toBe(pokemons[0].image);
 
     userEvent.click(screen.getByRole('button', { name: pokemonByType.type }));
+
+    const pokeName2 = screen.getByTestId('pokemon-name').textContent;
+    const pokeType2 = screen.getByTestId('pokemon-type').textContent;
+    const pokeWeigth2 = screen.getByText(/average weight/i).textContent;
+    const { averageWeight: weigth2 } = pokemonByType;
+    const pokeImg2 = screen.getByAltText(`${pokemonByType.name} sprite`);
+
+    expect(pokeName2).toBe(pokemonByType.name);
+    expect(pokeType2).toBe(pokemonByType.type);
+    expect(pokeWeigth2).toMatch(weigth2.value);
+    expect(pokeWeigth2).toMatch(weigth2.measurementUnit);
+    expect(pokeImg2.src).toBe(pokemonByType.image);
   });
 
   test('Se o card contem um link para os detalhes do pokemon', () => {
@@ -45,6 +60,11 @@ describe('6 - Renderize o card de um pokemon', () => {
       level: 2,
       name: regExName1,
     })).toBeInTheDocument();
+
+    expect(screen.getAllByAltText(`${pokemons[0].name} location`))
+      .toHaveLength(pokemons[0].foundAt.length);
+    expect(screen.getByText(pokemons[0].summary)).toBeInTheDocument();
+
     expect(history.location.pathname).toBe(`/pokemons/${pokemons[0].id}`);
 
     userEvent.click(screen.getByRole('link', { name: 'Home' }));
@@ -56,6 +76,11 @@ describe('6 - Renderize o card de um pokemon', () => {
       level: 2,
       name: regExName2,
     })).toBeInTheDocument();
+
+    expect(screen.getAllByAltText(`${pokemonByType.name} location`))
+      .toHaveLength(pokemonByType.foundAt.length);
+    expect(screen.getByText(pokemonByType.summary)).toBeInTheDocument();
+
     expect(history.location.pathname).toBe(`/pokemons/${pokemonByType.id}`);
   });
 });
