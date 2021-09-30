5cea41687eaeb20a8534eea5c462ea331380ad47
Leomar Linhares <leomar_linhares@hotmail.com>
2021-09-30 12:12:44 -0300

    refatora o requisito 5 para chamar renderWithRouter dentro de um beforeEach :rocket:

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -6,8 +6,9 @@ import App from '../App';
 
 describe('Teste do componente Pokedex', () => {
   const POKEMON_TYPE_TESTID = 'pokemon-type';
+  beforeEach(() => renderWithRouter(<App />));
+
   it('deveria conter um <h2> com o texto `Encountered pokémons`', () => {
-    renderWithRouter(<App />);
     const title = screen.getByRole('heading', {
       name: 'Encountered pokémons',
       level: 2,
@@ -16,7 +17,6 @@ describe('Teste do componente Pokedex', () => {
   });
 
   it('deveria mostrar o próximo Pokemon ao clicar em Próximo pokémon', () => {
-    renderWithRouter(<App />);
     const { innerHTML: firstPokemon } = screen.getByTestId(POKEMON_TYPE_TESTID);
     const nextButton = screen.getByRole('button', { name: 'Próximo pokémon' });
     userEvent.click(nextButton);
@@ -25,13 +25,11 @@ describe('Teste do componente Pokedex', () => {
   });
 
   it('deveria haver apenas 1 pokemon na página', () => {
-    renderWithRouter(<App />);
     const allPokemons = screen.getAllByTestId('pokemon-name');
     expect(allPokemons.length).toStrictEqual(1);
   });
 
   it('deveriam existir botões de filtro e os mesmos devem filtrar os pokemons', () => {
-    renderWithRouter(<App />);
     const filterButtons = screen.getAllByTestId('pokemon-type-button');
     filterButtons.forEach((button) => {
       userEvent.click(button);
