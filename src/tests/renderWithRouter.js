815749804a1a2233c0ae1937d3ab03196634e46d
Renata <renatateixeira.est@gmail.com>
2021-09-30 15:30:54 -0300

    Requisito 1 - falta testar notfound

--- src/tests/renderWithRouter.js
+++ src/tests/renderWithRouter.js
@@ -1,18 +1,19 @@
-import { render } from "@testing-library/react";
-import { Router } from "react-router-dom";
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 
 function renderWithRouter(component) {
   const costumHistory = createMemoryHistory();
   const selectorRLT = render(
-    <Router history = {costumHistory}>
+    <Router history={ costumHistory }>
       { component }
-    </Router>
+    </Router>,
   );
   return {
     ...selectorRLT,
     history: costumHistory,
-  }
+  };
 }
 
 export default renderWithRouter;
