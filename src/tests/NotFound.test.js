4c2c0249ae0282d29a958d5b1a6cb16d163f71b5
Mariana Ferreira <marianadesouzaferreira01@gmail.com>
2021-09-30 18:28:18 -0300

    Requisito 4 - NotFound.test.js

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testando o componente <NotFound.js />', () => {
+  it('Teste se página contém um h2 com o "texto Page requested not found"', () => {
+    render(<NotFound />);
+    const notFoundPage = screen.getByText('Page requested not found');
+    expect(notFoundPage).toBeDefined();
+  });
+  it('Verificar Crying Emoji', () => {
+    render(<NotFound />);
+    const emoji = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(emoji).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
