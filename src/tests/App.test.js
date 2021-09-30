26a57dfda352b34cbae0de537d928c0fcd70c8b6
Bruno M Landim <brunolandim13@hotmail.com>
2021-09-30 12:00:26 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,53 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('Teste para links e rotas no App.js', () => {
+  it('existe link home e se ao clicar leva para rota /', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+
+    const Home = screen.getByRole('link', { name: 'Home' });
+    expect(Home).toBeInTheDocument();
+
+    userEvent.click(Home);
+    expect(history.location.pathname).toBe('/');
+  });
+
+  it('existe link About e se ao clicar leva para rota /about', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+
+    const About = screen.getByRole('link', { name: 'About' });
+    expect(About).toBeInTheDocument();
+
+    userEvent.click(About);
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  it('existe link favorites e se ao clicar leva para rota /favorites ', () => {
+    const history = createMemoryHistory();
+    render(
+      <Router history={ history }>
+        <App />
+      </Router>,
+    );
+
+    const Favorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(Favorite).toBeInTheDocument();
+
+    userEvent.click(Favorite);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+});
