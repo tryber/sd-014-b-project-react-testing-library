2793ac57778cd9249e84204dbaf44cac3aa4ea51
Gustavo Sant'Anna <g_santanna@outlook.com>
2021-09-30 01:28:34 -0300

    Tentativa requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,61 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderRoute from '../helper/renderRoute';
+import pokemons from '../data';
+
+// https://stackoverflow.com/questions/43390873/template-literal-inside-of-the-regex
+describe('6 - Renderize o card de um pokemon', () => {
+  const regExName1 = new RegExp(String.raw`${pokemons[0].name} details`, 'i');
+  const pokemonByType = pokemons
+    .find((poke) => poke.type === pokemons[pokemons.length - 1].type);
+  const regExName2 = new RegExp(String.raw`${pokemonByType.name} details`, 'i');
+
+  test('Se o card é renderizado corretamente', () => {
+    const { history } = renderRoute('/');
+
+    userEvent.click(screen.getByRole('link', { name: /details/i }));
+
+    userEvent.click(screen.getByRole('checkbox', { checked: false }));
+    const starIcon = screen.getByAltText(`${pokemons[0].name} is marked as favorite`);
+    expect(starIcon).toBeInTheDocument();
+    expect(starIcon.src).toMatch(/star-icon.svg/);
+
+    userEvent.click(screen.getByRole('link', { name: 'Home' }));
+    const pokeName1 = screen.getByTestId('pokemon-name').textContent;
+    const pokeType1 = screen.getByTestId('pokemon-type').textContent;
+    const pokeWeigth1 = screen.getByText(/average weight/i).textContent;
+    const { averageWeight: weigth1 } = pokemons[0];
+    const pokeImg1 = screen.getByAltText(`${pokemons[0].name} sprite`);
+
+    expect(pokeName1).toBe(pokemons[0].name);
+    expect(pokeType1).toBe(pokemons[0].type);
+    expect(pokeWeigth1).toMatch(weigth1.value);
+    expect(pokeWeigth1).toMatch(weigth1.measurementUnit);
+    expect(pokeImg1.src).toBe(pokemons[0].image);
+
+    userEvent.click(screen.getByRole('button', { name: pokemonByType.type }));
+  });
+
+  test('Se o card contem um link para os detalhes do pokemon', () => {
+    const { history } = renderRoute('/');
+
+    userEvent.click(screen.getByRole('link', { name: /details/i }));
+
+    expect(screen.getByRole('heading', {
+      level: 2,
+      name: regExName1,
+    })).toBeInTheDocument();
+    expect(history.location.pathname).toBe(`/pokemons/${pokemons[0].id}`);
+
+    userEvent.click(screen.getByRole('link', { name: 'Home' }));
+
+    userEvent.click(screen.getByRole('button', { name: pokemonByType.type }));
+    userEvent.click(screen.getByRole('link', { name: /details/i }));
+
+    expect(screen.getByRole('heading', {
+      level: 2,
+      name: regExName2,
+    })).toBeInTheDocument();
+    expect(history.location.pathname).toBe(`/pokemons/${pokemonByType.id}`);
+  });
+});
