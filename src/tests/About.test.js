8de4c46eb68bed3665cbeb1f05329f49e28b7c06
Rodrigo Cavalheiro <oldcavalheiro@gmail.com>
2021-09-30 00:18:39 -0300

    req2 done tests in About component

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+// npx stryker run ./stryker/About.conf.json
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('teste do componente About', () => {
+  it('deve conter h2 com o texto About Pokédex', () => {
+    render(<About />);
+    const h2 = screen.getByRole('heading');
+    expect(h2.textContent).toBe('About Pokédex');
+  });
+
+  it('deve conter dois parágrafos com texto sobre Pokédex', () => {
+    render(<About />);
+    const paragraph0 = screen.getByText(/application simulates a Pokédex/);
+    const paragraph1 = screen.getByText(/filter Pokémons by type/);
+    expect(paragraph0).toBeInTheDocument();
+    expect(paragraph1).toBeInTheDocument();
+  });
+
+  it('deve conter imagem da pokédex com sua devida source', () => {
+    render(<About />);
+    const img = screen.getByAltText('Pokédex');
+    const src = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(img).toHaveAttribute('src', src);
+  });
+});
