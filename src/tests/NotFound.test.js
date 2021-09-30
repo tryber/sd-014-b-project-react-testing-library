a010fa6f90914b2dbca7c16f4e3c12302ecca2ac
LucasAccurcio <lucas.accurcio@gmail.com>
2021-09-30 12:51:38 -0300

    Requisito 04

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testing component NotFound', () => {
+  it('should test a not existent path', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pagina/que-nao-existe/');
+    const noMatch = screen.getByText(/Page requested not found/i);
+    expect(noMatch).toBeInTheDocument();
+  });
+
+  it('should have a image', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pagina/que-nao-existe/');
+    const noMatch = screen.getByText(/Page requested not found/i);
+    expect(noMatch).toBeInTheDocument();
+
+    const image = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
