d59c7595ebdbfc9dd8dab6a31a49b4dab0563115
Thales Lima <thales.bplima@gmail.com>
2021-09-30 21:54:56 -0300

    impl req3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Req3: Testing Component <FavoritePokemons />', () => {
+  test('if <FavoritePokemons /> displays "No favorite Pokemon found" when needed', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const notFound = screen.getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+
+  test('if <FavoritePokemons /> displays favorite pokemon properly', () => {
+    renderWithRouter(<App />);
+
+    const detailsLink = screen.getByRole('link', { name: 'More details' });
+    fireEvent.click(detailsLink);
+
+    const favoriteCheck = screen.getByRole('checkbox', {
+      name: 'Pokémon favoritado?',
+    });
+    fireEvent.click(favoriteCheck);
+
+    renderWithRouter(<FavoritePokemons />);
+
+    const favPokemon = screen.getByTestId('pokemon-name');
+    expect(favPokemon).toBeInTheDocument();
+  });
+});
