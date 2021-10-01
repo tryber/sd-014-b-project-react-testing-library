eaa31bdd84b105855a4343cc7401eabc46ad5285
Jucélio Brandão <juceneon@gmail.com>
2021-09-30 23:35:43 -0300

    Mockando Pokemons

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -4,7 +4,79 @@ import { createMemoryHistory } from 'history';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Pokedex } from '../components';
-import pokemons from '../data';
+
+const pokemons = [
+  {
+    id: 4,
+    name: 'Charmander',
+    type: 'Fire',
+    averageWeight: {
+      value: '8.5',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Charmander_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Alola Route 3',
+        map: 'https://cdn2.bulbagarden.net/upload/9/93/Alola_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 3',
+        map: 'https://cdn2.bulbagarden.net/upload/4/4a/Kanto_Route_3_Map.png',
+      },
+      {
+        location: 'Kanto Route 4',
+        map: 'https://cdn2.bulbagarden.net/upload/2/24/Kanto_Route_4_Map.png',
+      },
+      {
+        location: 'Kanto Rock Tunnel',
+        map: 'https://cdn2.bulbagarden.net/upload/6/6f/Kanto_Rock_Tunnel_Map.png',
+      },
+    ],
+    summary: 'The flame on its tail shows the strength of its life force.',
+  },
+  {
+    id: 23,
+    name: 'Ekans',
+    type: 'Poison',
+    averageWeight: {
+      value: '6.9',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/1/18/Spr_5b_023.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Ekans_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Goldenrod Game Corner',
+        map: 'https://cdn2.bulbagarden.net/upload/e/ec/Johto_Goldenrod_City_Map.png',
+      },
+    ],
+    summary: 'It can freely detach its jaw to swallow large prey whole.',
+  },
+  {
+    id: 78,
+    name: 'Rapidash',
+    type: 'Fire',
+    averageWeight: {
+      value: '95.0',
+      measurementUnit: 'kg',
+    },
+    image: 'https://cdn2.bulbagarden.net/upload/5/58/Spr_5b_078.png',
+    moreInfo: 'https://bulbapedia.bulbagarden.net/wiki/Rapidash_(Pok%C3%A9mon)',
+    foundAt: [
+      {
+        location: 'Kanto Route 28',
+        map: 'https://cdn2.bulbagarden.net/upload/5/5b/Kanto_Route_28_Map.png',
+      },
+      {
+        location: 'Johto Mount Silver',
+        map: 'https://cdn2.bulbagarden.net/upload/9/95/Johto_Mt_Silver_Map.png',
+      },
+    ],
+    summary: 'At full gallop, its four hooves barely touch the ground.',
+  },
+];
 
 const renderWithRouter = () => {
   const history = createMemoryHistory();
@@ -29,13 +101,18 @@ describe('Testa se o componente Pokedex:', () => {
 
   test('O próximo Pokémon é listado quando o botão é clicado', () => {
     renderWithRouter();
-    const buttonNextPoke = screen.getByText('Próximo pokémon');
+    const buttonNextPoke = screen.getByRole('button', { name: 'Próximo pokémon' });
     expect(buttonNextPoke).toBeInTheDocument();
     const firstPoke = screen.getByText(pokemons[0].name);
     expect(firstPoke).toBeInTheDocument();
     userEvent.click(buttonNextPoke);
     const secondPoke = screen.getByText(pokemons[1].name);
     expect(secondPoke).toBeInTheDocument();
+    userEvent.click(buttonNextPoke);
+    const thirdPoke = screen.getByText(pokemons[2].name);
+    expect(thirdPoke).toBeInTheDocument();
+    userEvent.click(buttonNextPoke);
+    expect(firstPoke).toBeInTheDocument();
   });
 
   test('É mostrado apenas um pokemon por vez', () => {
@@ -47,10 +124,10 @@ describe('Testa se o componente Pokedex:', () => {
 
   test('contem os botões de filto', () => {
     renderWithRouter();
-    const NUM_OF_TYPE_BUTTONS = 7;
+    const NUM_OF_TYPE_BUTTONS = 2;
     const typeButtons = screen.getAllByTestId('pokemon-type-button');
     expect(typeButtons.length).toBe(NUM_OF_TYPE_BUTTONS);
-    expect(typeButtons[0]).toHaveTextContent('Electric');
+    expect(typeButtons[0]).toHaveTextContent('Fire');
 
     const nextPokeTestId = 'next-pokemon';
 
@@ -62,11 +139,9 @@ describe('Testa se o componente Pokedex:', () => {
     expect(ekansPoke).toBeInTheDocument();
 
     userEvent.click(typeButtons[0]);
-    expect(screen.getByTestId(nextPokeTestId)).toBeDisabled();
-    userEvent.click(typeButtons[2]);
-    expect(screen.getByTestId(nextPokeTestId)).toBeDisabled();
-    userEvent.click(typeButtons[4]);
     expect(screen.getByTestId(nextPokeTestId)).toBeEnabled();
+    userEvent.click(typeButtons[1]);
+    expect(screen.getByTestId(nextPokeTestId)).toBeDisabled();
   });
 
   test('contém um botão para resetar o filtro', () => {
@@ -76,8 +151,8 @@ describe('Testa se o componente Pokedex:', () => {
 
     const nextPoke = screen.getByRole('button', { name: 'Próximo pokémon' });
     userEvent.click(nextPoke);
-    expect(screen.getByTestId('pokemon-type')).toHaveTextContent('Fire');
+    expect(screen.getByTestId('pokemon-type')).toHaveTextContent('Poison');
     userEvent.click(nextPoke);
-    expect(screen.getByTestId('pokemon-type')).toHaveTextContent('Bug');
+    expect(screen.getByTestId('pokemon-type')).toHaveTextContent('Fire');
   });
 });
