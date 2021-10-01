f843945a8a1f4943226198f6c20eadabe460eb2a
Vitor Silva <vitor.o.silva95@gmail.com>
2021-10-01 15:01:36 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente NotFound.', () => {
+  test('se página contém um heading h2 com o texto Page requested not found', () => {
+    render(<NotFound />);
+    const notFound = screen.getByRole('heading', {
+      level: 2,
+      name: /Page requested not found/,
+    });
+    expect(notFound).toBeInTheDocument();
+  });
+
+  test('se existe a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    render(<NotFound />);
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const img = screen.getAllByRole('img');
+
+    expect(img[1].getAttribute('src')).toEqual(url);
+  });
+});
