b46aa1413364baf853cd2cd54c64d5f8cd805445
Renata <renatateixeira.est@gmail.com>
2021-10-01 14:41:55 -0300

    Requisito 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+// import React from 'react';
+// import { screen, fireEvent } from '@testing-library/react';
+// import renderWithRouter from './renderWithRouter';
+// import App from '../App';
+// import MutationObserver from '@sheerun/mutationobserver-shim';
+// window.MutationObserver = MutationObserver;
+
+// describe('Testado o componente FavoritePokemons', () => {
+//   test(`Se não houver pokémons favoritos, deve aparecer
+//   'No favorite pokemon found'`, () => {
+//     renderWithRouter(<App />);
+//     const pathname = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+//     fireEvent.click(pathname);
+//     const frase = screen.getByText('No favorite pokemon found');
+//     expect(frase).toBeInTheDocument();
+//   });
+// });
