4cfbd7f08518aca269b88ee4716775b644d4b0b8
Vitor Silva <vitor.o.silva95@gmail.com>
2021-10-01 14:59:46 -0300

    Começando requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+
+describe('testa o componente FavoritePokemons', () => {
+  test('se é exibido a mensagem No favorite pokemon found', () => {
+    render(<FavoritePokemons />);
+    const noFavotirePokemon = screen.getByText('No favorite pokemon found');
+    expect(noFavotirePokemon).toBeInTheDocument();
+  });
+});
