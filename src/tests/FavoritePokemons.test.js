e38850e8c9b661904662ba8ba31b5806c365a8f4
andremorsch <andremorsch37@gmail.com>
2021-10-01 17:09:13 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+
+describe('Teste Favorite Pokemons', () => {
+  it('Mensagem de não encontrado', () => {
+    render(<FavoritePokemons />);
+    const notFound = screen.getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+});
