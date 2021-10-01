00c9b3cb3a6f040774c0bd1f13ad2c7d41f43993
andremorsch <andremorsch37@gmail.com>
2021-10-01 18:49:20 -0300

    requusito 5 - teste pq não ta indo o result aqui

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './utils/renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Teste Pokedex', () => {
+  it('verifica h2', () => {
+    renderWithRouter(<App />);
+
+    const h2pokemons = screen.getByRole('heading', {
+      name: 'Encountered pokémons',
+      level: 2,
+    });
+    expect(h2pokemons).toBeInTheDocument();
+  });
+});
