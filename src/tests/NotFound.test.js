0da815d2d2d4003cfb7ba9c1c37c7ad701978d01
andremorsch <andremorsch37@gmail.com>
2021-10-01 17:51:53 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Teste NotFFound', () => {
+  it('Verifica a h2 e imagem', () => {
+    render(<NotFound />);
+    const notFound = screen.getByRole('heading', {
+      name: /Page requested not found/,
+      level: 2,
+    });
+    expect(notFound).toBeInTheDocument();
+
+    const gif = screen.getByAltText(/Pikachu crying/);
+    expect(gif).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
