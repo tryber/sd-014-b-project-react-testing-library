67646676e25283df92997a03d50b4a153ae41098
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-02 14:40:37 -0400

    Requisito 1 - Refatora o teste do componente App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,21 +3,22 @@ import { screen } from '@testing-library/react';
 import renderWithRouter from '../uteis/renderWithRouter';
 import App from '../App';
 
+beforeEach(() => {
+  renderWithRouter(<App />);
+});
+
 describe('Testa o componente app', () => {
   test('se o link "Home" aparece na tela', () => {
-    renderWithRouter(<App />);
     const textHome = screen.getByText('Home');
     expect(textHome).toBeInTheDocument();
   });
 
   test('se o link "About" aparece na tela', () => {
-    renderWithRouter(<App />);
     const textAbout = screen.getByText('About');
     expect(textAbout).toBeInTheDocument();
   });
 
   test('se o link "Favorite Pokémons" aparece na tela', () => {
-    renderWithRouter(<App />);
     const textFavoritePokemon = screen.getByText('Favorite Pokémons');
     expect(textFavoritePokemon).toBeInTheDocument();
   });
