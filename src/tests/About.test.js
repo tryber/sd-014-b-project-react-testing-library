7e4bb63117db98c01ac897aee017f9dfd2ae9854
Elithon Silva <elithonsilva@gmail.com>
2021-10-02 16:24:50 -0300

    testa se a imagem é exibida com o link correto

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -18,4 +18,10 @@ describe('se a página contém as informações sobre a Pokédex', () => {
     expect(firstParagraph).toBeInTheDocument();
     expect(secondParagraph).toBeInTheDocument();
   });
+  it('deveria exibir a seguinte imagem de uma Pokédex', () => {
+    render(<About />);
+    const image = screen.getByRole('img');
+
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });
