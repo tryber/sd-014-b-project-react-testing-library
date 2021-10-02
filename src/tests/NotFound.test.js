48831d610afab99017c5dbd33bebae7a43121b8f
Elithon Silva <elithonsilva@gmail.com>
2021-10-02 18:20:45 -0300

    cria teste que verifica heading h2 no componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('verifica se contém um heading e uma imagem', () => {
+  it('deveria exibir um heading "h2" com texto "Page requested not found"', () => {
+    render(<NotFound />);
+    const textTitle = screen.getByText(/Page requested not found/i);
+    expect(textTitle).toBeInTheDocument();
+  });
+});
