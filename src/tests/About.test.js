05f67fd645f0ede14b3c40c8721f176f6ccd77cb
Elithon Silva <elithonsilva@gmail.com>
2021-10-02 12:45:24 -0300

    impl: testa se exibe um h2 com texto About Pokédex

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('se a página contém as informações sobre a Pokédex', () => {
+  it('deveria exibir um heading h2 com o texto About Pokédex', () => {
+    render(<About />);
+    const titleText = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(titleText).toBeInTheDocument();
+  });
+  // it('deveria exibir dois parágrafos com texto sobre a Pokédex', () => { });
+  // it('deveria exibir a seguinte imagem de uma Pokédex', () => { });
+});
