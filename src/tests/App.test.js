c048a6c5d26792d81e28996c764d4b1dceda911b
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-02 12:54:28 -0400

    Requisito 1 - Testa o componente App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -9,4 +9,16 @@ describe('Testa o componente app', () => {
     const textHome = screen.getByText('Home');
     expect(textHome).toBeInTheDocument();
   });
+
+  test('se o link "About" aparece na tela', () => {
+    renderWithRouter(<App />);
+    const textAbout = screen.getByText('About');
+    expect(textAbout).toBeInTheDocument();
+  });
+
+  test('se o link "Favorite Pokémons" aparece na tela', () => {
+    renderWithRouter(<App />);
+    const textFavoritePokemon = screen.getByText('Favorite Pokémons');
+    expect(textFavoritePokemon).toBeInTheDocument();
+  });
 });
