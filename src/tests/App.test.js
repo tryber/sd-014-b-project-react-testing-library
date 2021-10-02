b17c3f3abe778109115b97026df089039fced9e2
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-02 12:28:59 -0400

    Cria o renderWithRouter

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('Testa o componente app', () => {
+  test('Testa o link', () => {
+    render(<App />);
+    const textHome = screen.getByText('Home');
+    expect(textHome).toBeInTheDocument();
+  });
+});
