b6c78c5e23ee9c40dacc3204c9f1713c823fd0a6
Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-10-02 22:09:48 -0300

    Implementa requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,52 @@
-test('', () => {});
+// Referência, PR do Rafael Veiga
+// src: https://github.com/tryber/sd-014-b-project-react-testing-library/pull/30/files
+
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './helpers/renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <App.js />', () => {
+  it(`Teste se o topo da aplicação
+  contém um conjunto fixo de links de navegação.`, () => {
+    renderWithRouter(<App />);
+    const links = screen.getAllByRole('link');
+    expect(links[0]).toHaveTextContent('Home');
+    expect(links[1]).toHaveTextContent('About');
+    expect(links[2]).toHaveTextContent('Favorite Pokémons');
+  });
+
+  // src, alt: https://stackoverflow.com/a/51334009
+  it(`Teste se a aplicação é redirecionada para a página inicial,
+  na URL / ao clicar no link Home da barra de navegação.`, () => {
+    const { history } = renderWithRouter(<App />);
+    const links = screen.getAllByRole('link');
+    userEvent.click(links[0]);
+    expect(history.location.pathname).toBe('/');
+  });
+
+  it(`Teste se a aplicação é redirecionada para a página de About,
+  na URL /about, ao clicar no link About da barrade navegação.`, () => {
+    const { history } = renderWithRouter(<App />);
+    const links = screen.getAllByRole('link');
+    userEvent.click(links[1]);
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  it(`Teste se a aplicação é redirecionada para a página de Pokémons Favoritados,
+  na URL /favorites, ao clicar no link Favorite Pokémons da barra de navegação.`, () => {
+    const { history } = renderWithRouter(<App />);
+    const links = screen.getAllByRole('link');
+    userEvent.click(links[2]);
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  it(`Teste se a aplicação é redirecionada para a página
+  Not Found ao entrar em uma URL desconhecida.`, () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('lorem-ipsum');
+    const page = screen.getByText(/requested not found/);
+    expect(page).toBeInTheDocument();
+  });
+});
