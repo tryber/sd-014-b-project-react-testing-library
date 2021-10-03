f30b556039d7e25e2e9ed8a852beac051d98869e
Bruno Trindade <brunotrndd@gmail.com>
2021-10-03 13:03:10 -0300

    Requisito 1 parcial

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,3 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
 
-// commit incial
+describe('Testando o componente App', () => {
+  it('se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+    renderWithRouter(<App />);
+    const home = screen.getByText('Home');
+    const about = screen.getByText('About');
+    const favorite = screen.getByText('Favorite Pokémons');
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favorite).toBeInTheDocument();
+  });
+
+  it(`se a aplicação é redirecionada para a página inicial,
+  na URL / ao clicar no link Home da barra de navegação.`, () => {
+    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
+    const home = screen.getByText('Home');
+    userEvent.click(home);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+});
