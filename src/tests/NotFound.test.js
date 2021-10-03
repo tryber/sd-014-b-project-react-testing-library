6b77cbfd2ff571809658f784fba8ebbe980d1dc8
Bruno Trindade <brunotrndd@gmail.com>
2021-10-03 18:15:48 -0300

    Requisito 4 completo

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testando o componente FavoritePokemons', () => {
+  it('se página contém um heading h2 com o texto Page requested not found 😭', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pagina/que-nao-existe/');
+    const heading = screen.getByRole('heading', { level: 2 });
+    expect(heading).toHaveTextContent('Page requested not found 😭');
+  });
+
+  it('se página mostra a imagem'
+  + 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pagina/que-nao-existe/');
+    const image = screen.getAllByRole('img');
+    expect(image[1]).toHaveAttribute(
+      'src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    );
+  });
+});
