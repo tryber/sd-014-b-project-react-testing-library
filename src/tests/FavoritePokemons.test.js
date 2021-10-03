f89ee960e20c9563a1ac7435e53d69fbd9ee5817
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-02 20:47:54 -0400

    Requisito 3 - Testa o componente FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../uteis/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+beforeEach(() => {
+  renderWithRouter(<FavoritePokemons />);
+});
+
+describe('Testa o componente FavoritePokemons', () => {
+  test(`se é exibido na tela a mensagem "No favorite pokemon found",
+  se a pessoa não tiver pokémons favoritos.`, () => {
+    const textNoFavoritePokemons = screen.getByText('No favorite pokemon found');
+    expect(textNoFavoritePokemons).toBeInTheDocument();
+  });
+});
