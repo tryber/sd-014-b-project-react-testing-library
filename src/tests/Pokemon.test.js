9c7563e2fb2dbe16ddbc96fd7923c8e8fc77e6dc
vinicius-mns <vinicius_mns@hotmail.com>
2021-10-03 21:27:33 -0300

    requisito numero 7

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,31 @@
 test('', () => {});
+
+// Desisti desse e fui para o requisito 7
+
+// describe('Renderiza um card do pokemon', () => {
+//   it('contendo nome correto', () => {
+
+//   });
+//   it('contendo tipo correto', () => {
+
+//   });
+//   it('contendo peso medio do pokemon', () => {
+
+//   });
+//   it('contendo a imagem', () => {
+
+//   });
+//   it('contendo um link com mais detalhes sobre', () => {
+
+//   });
+// });
+
+// describe('Ao clickar no link com mais detalhes', () => {
+//   it('deverá ser redirecionado para páginas de detalhes', () => {
+
+//   });
+
+//   it('altera a url do navegador para o path igual ao id do pokemon', () => {
+
+//   });
+// });
