a967067f483196ac3f394a8b5c68dd05750c6961
Danilo <danilo.santossouza@outlook.com>
2021-10-04 14:12:27 -0300

    Requiquisito 3 resolvido

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,11 +1,13 @@
-// import userEvent from '@testing-library/user-event';
-// import { screen } from '@testing-library/react';
-// import React from 'react';
-// import renderWithRouter from './renderWithRouter';
-// import App from '../App';
+import { screen } from '@testing-library/react';
+import React from 'react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
 
 describe('Testa a aplicação do component FavoritePokemons', () => {
-  test('', () => {
-
+  test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+    const title = screen.getByText(/No favorite pokemon found/);
+    expect(title).toBeInTheDocument();
   });
 });
