9f1874786936a861360359bd66200fbe541861de
Vitor Silva <vitor.o.silva95@gmail.com>
2021-10-04 17:38:44 -0300

    Consertando erro lint

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -4,6 +4,8 @@ import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
+const moreDetails = 'More details';
+
 describe('Testa o componente Pokemon', () => {
   test('se é renderizado um card com as informações de determinado pokémon', () => {
     renderWithRouter(<App />);
@@ -23,7 +25,7 @@ describe('Testa o componente Pokemon', () => {
 
   test('se o card tem um link para exibir detalhes do Pokémon', () => {
     const { history } = renderWithRouter(<App />);
-    const linkMoreDetails = screen.getByRole('link', { name: 'More details' });
+    const linkMoreDetails = screen.getByRole('link', { name: moreDetails });
     expect(linkMoreDetails).toBeInTheDocument();
 
     userEvent.click(linkMoreDetails);
@@ -32,7 +34,7 @@ describe('Testa o componente Pokemon', () => {
 
   test('se clicar no link, é redirecionado para a página de detalhes de Pokémon.', () => {
     renderWithRouter(<App />);
-    const linkMoreDetails = screen.getByRole('link', { name: 'More details' });
+    const linkMoreDetails = screen.getByRole('link', { name: moreDetails });
     userEvent.click(linkMoreDetails);
 
     const h2 = screen.getByRole('heading', { level: 2, name: 'Pikachu Details' });
@@ -41,7 +43,7 @@ describe('Testa o componente Pokemon', () => {
 
   test('se existe um ícone de estrela nos Pokémons favoritados.', () => {
     renderWithRouter(<App />);
-    const linkMoreDetails = screen.getByRole('link', { name: 'More details' });
+    const linkMoreDetails = screen.getByRole('link', { name: moreDetails });
     userEvent.click(linkMoreDetails);
 
     const checkBoxfavorits = screen.getByRole('checkbox');
