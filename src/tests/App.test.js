d83fe41dec73463eefd45e9c26d121d1466305e3
eliHC <leonardohcds@gmail.com>
2021-10-04 16:09:11 -0300

    req 3

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-
 import renderWithRouter from './renderWithRouter';
 
 import App from '../App';
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,36 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('FavoritePokemons.js testcase', () => {
+  test(`se é exibido na tela a mensagem No favorite pokemon found,
+        se a pessoa não tiver pokémons favoritos.`, () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const noFavorites = screen.getByText(/no favorite pokemon found/i);
+    expect(noFavorites).toBeInTheDocument();
+  });
+
+  test('se é exibido todos os cards de pokémons favoritados.', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/pokemons/151');
+    const favoriteMew = screen.getByRole('checkbox', { id: 'favorite' });
+    userEvent.click(favoriteMew);
+
+    history.push('/pokemons/148');
+    const favoriteDragonair = screen.getByRole('checkbox', { id: 'favorite' });
+    userEvent.click(favoriteDragonair);
+
+    history.push('/favorites');
+
+    const markedAsFavorite = screen.getAllByRole('link', {
+      name: /more details/i,
+    });
+    expect(markedAsFavorite.length).toEqual(2);
+  });
+});
