b6c63ecbf2fdb59a9198f1809d8a72616e871265
Cecilia Cacais <ceci.cacais@gmail.com>
2021-10-04 15:28:21 -0300

    requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testando componente Pokemon', () => {
+  test('se é renderizado um card com as informações de determinado pokémon', () => {
+    renderWithRouter(<App />);
+    const imagemPikachu = screen.getByRole('img');
+    expect(imagemPikachu).toHaveAttribute('src',
+      'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+});
