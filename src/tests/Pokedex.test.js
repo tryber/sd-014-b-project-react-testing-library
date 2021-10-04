4d5e04ac17beb160705713b56930de4feb89f90b
enzo <enzo@pop-os.localdomain>
2021-10-04 11:10:24 -0300

    Requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,70 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, getByText, screen } from '@testing-library/react';
+import renderWithRouter from '../../helper/renderWithRouter';
+import App from '../App';
+
+const assert = require('assert');
+
+describe('Testa o componente Pokedex.js', () => {
+  test('Testa se página contém um heading h2 com o texto Encountered pokémons', () => {
+    renderWithRouter(<App />);
+
+    const title = screen.getByRole('heading', { level: 2, name: 'Encountered pokémons' });
+    expect(title).toBeInTheDocument();
+  });
+  test(`Teste se é exibido o próximo Pokémon da lista
+   quando o botão Próximo pokémon é clicado.`, () => {
+    renderWithRouter(<App />);
+
+    const button = screen.getByText('Próximo pokémon');
+
+    const pikachu = screen.getByText('Pikachu');
+    expect(pikachu).toBeInTheDocument();
+
+    fireEvent.click(button);
+
+    const charmander = screen.getByText('Charmander');
+    expect(charmander).toBeInTheDocument();
+
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
+
+    const afterLastPokemon = screen.getByText('Pikachu');
+    expect(afterLastPokemon).toBeInTheDocument();
+  });
+  test('Teste se é mostrado apenas um Pokémon por vez.', () => {
+    renderWithRouter(<App />);
+
+    const detailsTextPokemonOne = screen.getAllByText('More details');
+    expect(detailsTextPokemonOne.length).toBe(1);
+  });
+  test('Teste se a Pokédex tem os botões de filtro.', () => {
+    renderWithRouter(<App />);
+
+    const allPokemonsButton = screen.getByText('All');
+    fireEvent.click(allPokemonsButton);
+    expect(allPokemonsButton).toBeInTheDocument();
+
+    const filterButtons = screen.getAllByTestId('pokemon-type-button');
+    assert.strictEqual(filterButtons[0].textContent, 'Electric');
+    assert.strictEqual(filterButtons[1].textContent, 'Fire');
+    assert.strictEqual(filterButtons[2].textContent, 'Bug');
+    assert.strictEqual(filterButtons[3].textContent, 'Poison');
+    assert.strictEqual(filterButtons[4].textContent, 'Psychic');
+    assert.strictEqual(filterButtons[5].textContent, 'Normal');
+    assert.strictEqual(filterButtons[6].textContent, 'Dragon');
+
+    fireEvent.click(filterButtons[2]);
+    const caterpie = screen.getByText('Caterpie');
+    expect(caterpie).toBeInTheDocument();
+
+    const allPokemonsButtonTest2 = screen.getByText('All');
+    expect(allPokemonsButtonTest2).toBeInTheDocument();
+  });
+});
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../../helper/renderWithRouter';
+import Pokemon from '../components/Pokemon';
+import App from '../App';
+
+describe('Testa o componente Pokedex.js', () => {
+  test('', () => {
+    renderWithRouter(<App />);
+  });
+  test('', () => {});
+  test('', () => {});
+  test('', () => {});
+});
