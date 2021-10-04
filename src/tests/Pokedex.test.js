558b2da466de81d6efbe983a47009e3692cf64f1
Elithon Silva <elithonsilva@gmail.com>
2021-10-04 17:09:46 -0300

    impl: teste que verifica h2 na pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <Pokedex.js />', () => {
+  it('se página contém um heading h2 com o texto Encountered pokémons', () => {
+    renderWithRouter(<App />);
+    const titleText = screen.getByRole(
+      'heading', { level: 2, name: /Encountered pokémons/i },
+    );
+    expect(titleText).toBeInTheDocument();
+  });
+});
