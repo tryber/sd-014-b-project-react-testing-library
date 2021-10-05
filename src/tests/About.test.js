4ebe84293ea441f43bcbe8aaa86b31366945b873
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-10-04 21:42:08 -0300

    Teste se a página contém as informações sobre a Pokédex.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { About } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente <About.js />', () => {
+  it('Teste se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const infPokedex = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(infPokedex).toBeInTheDocument();
+  });
+  
+});
