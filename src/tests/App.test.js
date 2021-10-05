0d6e1c55d9e68ccbdea53b71599f581ef968dafc
GustavoPD <gudalmoro@gmail.com>
2021-10-05 03:15:38 -0300

    Criei o componente renderWithRouter e testei se o conjunto de links eram renderizados na pagina

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
-import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 describe('Testa a aplicação App', () => {
