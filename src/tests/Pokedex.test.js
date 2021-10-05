b72c83bc3dfecfdf5e9a75780bb0fae577a0cfd7
Leandro Xavier <leandrosxavi@gmail.com>
2021-10-05 00:09:38 -0300

    Correcao erros lint

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import renderWithRouter from '../helpers/renderWithRouter';
-import { Pokedex } from '../components';
+// import { Pokedex } from '../components';
+import App from '../App';
 
 describe('Testando o componente Pokedex.js', () => {
   it('Verifica se contém um h2 com "Encountered pokémons"', () => {
-    renderWithRouter(<Pokedex />);
+    renderWithRouter(<App />); // render do Pokedex dá erro na função filterPokemon.
     const encounteredText = screen.getByRole('heading', {
       name: 'Encountered pokémons',
       level: 2,
