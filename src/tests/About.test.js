56f5bce59286f8d367e9a32b795c22eca5219cd5
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-10-04 22:46:39 -0300

    testando a renderização de uma iagem específica

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,14 +3,20 @@ import { screen } from '@testing-library/react';
 import { About } from '../components';
 import renderWithRouter from './renderWithRouter';
 
-describe('Teste o componente <About.js />', () => {
-  it('Teste se a página contém as informações sobre a Pokédex', () => {
+describe('Teste se a página contém as informações sobre a Pokédex', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
     renderWithRouter(<About />);
-    const infPokedex = screen.getByRole('heading', {
+    const heading = screen.getByRole('heading', {
       level: 2,
       name: 'About Pokédex',
     });
-    expect(infPokedex).toBeInTheDocument();
+    expect(heading).toBeInTheDocument();
   });
-  
+
+  it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const image = screen.getByAltText('Pokédex');
+    expect(image).toHaveAttribute('src',
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
 });
