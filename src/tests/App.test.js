11c3018762b1a4bc26e4b0ca97cb315cc8452362
GustavoPD <gudalmoro@gmail.com>
2021-10-05 03:12:54 -0300

    Criei o componente renderWithRouter e testei se o conjunto de links eram renderizados na pagina

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,24 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+
+describe('Testa a aplicação App', () => {
+  test('Se é renderizado o conjunto de links Home, About e Favorite Pokémons', () => {
+    renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', {
+      name: 'Home',
+    });
+    const aboutLink = screen.getByRole('link', {
+      name: 'About',
+    });
+    const favPokeLink = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+    expect(homeLink).toBeInTheDocument();
+    expect(aboutLink).toBeInTheDocument();
+    expect(favPokeLink).toBeInTheDocument();
+  });
+});
