b0f3793b94193cce6da56c47d69f3d4459290ec7
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-10-05 11:00:36 -0300

    trocando de getByAltText para ByRole img

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -15,7 +15,7 @@ describe('Teste se a página contém as informações sobre a Pokédex', () => {
 
   it('Teste se a página contém a seguinte imagem de uma Pokédex', () => {
     renderWithRouter(<About />);
-    const image = screen.getByAltText('Pokédex');
+    const image = screen.getByRole('img');
     expect(image).toHaveAttribute('src',
       'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+it('', () => {});
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// import { Pokedex } from '../components';
+// import renderWithRouter from './renderWithRouter';
+
+// describe(' Teste o componente <Pokedex.js />', () => {
+//   it('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+//     renderWithRouter(<Pokedex />);
+//     const heading = screen.getByRole('heading', {
+//       level: 2,
+//       name: 'Encountered pokémons',
+//     });
+//     expect(heading).toBeInTheDocument();
+//   });
+// });
