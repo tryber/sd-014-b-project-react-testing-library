5e0b8df8f662ac1dddcfd60fdab6ba3359e3325e
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-10-04 23:57:12 -0300

    Teste se página contém um heading h2 com o texto Page requested not found

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('', () => {
+  test('', () => {
+    renderWithRouter(<NotFound />);
+    const heading = screen.getByRole('heading', {
+      level: 2,
+      name: (/Page requested not found/i),
+    });
+    expect(heading).toBeInTheDocument();
+  });
+});
