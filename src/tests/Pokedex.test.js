a9636453ad944c05ffde2fe6099abb9eed160c11
Leandro Xavier <leandrosxavi@gmail.com>
2021-10-05 00:06:43 -0300

    Adiciona requisito 6

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import { Pokedex } from '../components';
+
+describe('Testando o componente Pokedex.js', () => {
+  it('Verifica se contém um h2 com "Encountered pokémons"', () => {
+    renderWithRouter(<Pokedex />);
+    const encounteredText = screen.getByRole('heading', {
+      name: 'Encountered pokémons',
+      level: 2,
+    });
+    expect(encounteredText).toBeInTheDocument();
+  });
+});
