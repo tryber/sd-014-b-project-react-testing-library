1f574cb945c743ee7e3b0e326defd3c37153ef8e
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-10-05 00:35:10 -0300

    testandoa  imagem do componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -3,13 +3,23 @@ import { screen } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import NotFound from '../components/NotFound';
 
-describe('', () => {
-  test('', () => {
-    renderWithRouter(<NotFound />);
-    const heading = screen.getByRole('heading', {
-      level: 2,
-      name: (/Page requested not found/i),
+describe('Teste o componente <NotFound.js />', () => {
+  test('Teste se página contém um heading h2 com o texto Page requested not found',
+    () => {
+      renderWithRouter(<NotFound />);
+      const heading = screen.getByRole('heading', {
+        level: 2,
+        name: (/Page requested not found/i),
+      });
+      expect(heading).toBeInTheDocument();
     });
-    expect(heading).toBeInTheDocument();
+
+  test('Teste se página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+    const image = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(image).toHaveAttribute('src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
   });
 });
