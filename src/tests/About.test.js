7cc7f805fb6a2502fc4031a2a13fd927013099ce
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-10-04 23:44:13 -0300

    Teste se a página contém dois parágrafos com texto sobre a PokédexTeste se a página contém as informações sobre a Pokédex.

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -19,4 +19,10 @@ describe('Teste se a página contém as informações sobre a Pokédex', () => {
     expect(image).toHaveAttribute('src',
       'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
+
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const paragraph = screen.getAllByText(/Pokémons/i);
+    expect(paragraph.length).toBe(2);
+  });
 });
