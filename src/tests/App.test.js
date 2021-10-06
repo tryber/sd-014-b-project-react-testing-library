10b77dac642a164e1fe2f70e7965222543255925
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-10-06 12:07:49 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+
+test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  RouterRender(<App />);
+  const homeLink = screen.getByRole('link', { name: 'Home' });
+  const aboutLink = screen.getByRole('link', { name: 'About' });
+  const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+  expect(homeLink).toBeInTheDocument();
+  expect(aboutLink).toBeInTheDocument();
+  expect(favoriteLink).toBeInTheDocument();
+});
