a43958e8f69ff07ce77230ac8086835da73b48fe
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-10-06 12:24:32 -0300

    cria componente RenderWithRouter

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import App from '../App';
+import RenderWithRouter from '../RenderWithRouter';
 
 test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
-  RouterRender(<App />);
+  RenderWithRouter(<App />);
   const homeLink = screen.getByRole('link', { name: 'Home' });
   const aboutLink = screen.getByRole('link', { name: 'About' });
   const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
