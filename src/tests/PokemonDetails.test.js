82d8e20deffa0bf8c374597eae62d84fdf54bd6b
Paulo França <paulovinicius.franca@gmail.com>
2021-10-05 21:23:31 -0300

    Implementa a parte um do requisito 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../components/renderWithRouter';
+
+describe('Testa o PokemonDetails', () => {
+  test('Testa se as informações detalhadas são mostradas na tela', () => {
+    renderWithRouter(<App />);
+    const moreDetailsLink = screen.getByRole('link', { name: 'More details' });
+    expect(moreDetailsLink).toBeInTheDocument();
+    userEvent.click(moreDetailsLink);
+    expect(moreDetailsLink).not.toBeInTheDocument();
+    const pokemonDetailsText = screen.getByRole('heading', {
+      level: 2,
+      name: 'Pikachu Details',
+    });
+    expect(pokemonDetailsText).toBeInTheDocument();
+    const summaryHeading = screen.getByRole('heading', {
+      level: 2,
+      name: 'Summary',
+    });
+    expect(summaryHeading).toBeInTheDocument();
+    const summaryParagraph = screen.getByText('This intelligent', { exact: false });
+    expect(summaryParagraph).toBeInTheDocument();
+  });
+});
