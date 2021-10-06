64064a92392e4504f9ac2580cf8f0881a7115757
Paulo França <paulovinicius.franca@gmail.com>
2021-10-05 22:01:57 -0300

    Implementa o requisito 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -3,6 +3,7 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../components/renderWithRouter';
+import pokemons from '../data';
 
 describe('Testa o PokemonDetails', () => {
   test('Testa se as informações detalhadas são mostradas na tela', () => {
@@ -23,5 +24,29 @@ describe('Testa o PokemonDetails', () => {
     expect(summaryHeading).toBeInTheDocument();
     const summaryParagraph = screen.getByText('This intelligent', { exact: false });
     expect(summaryParagraph).toBeInTheDocument();
+  });
+
+  test('Testa se existe na página uma seção com os mapas', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const gameLocationHeading = screen.getByRole('heading', {
+      level: 2,
+      name: 'Game Locations of Pikachu',
+    });
+    expect(gameLocationHeading).toBeInTheDocument();
+    const locationImage = screen.getAllByRole('img', {
+      alt: 'Pikachu location',
+    });
+    expect(locationImage[1].alt).toBe('Pikachu location');
+    expect(locationImage[1].src).toBe('https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+  });
+
+  test('Testa se o usuário pode favoritar um pokémon através da página', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const checkboxIput = screen.getByRole('checkbox', {
+      name: 'Pokémon favoritado?',
+    });
+    expect(checkboxIput).toBeInTheDocument();
   });
 });
