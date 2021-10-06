41b980b7174f7bae35c032856774181f3bc1384d
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2021-10-06 01:29:06 -0300

    Requisito 3 OK

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('03 - Teste o componente <FavoritePokemons.js />', () => {
+  test('a) Se No favorite pokemon found aparece quando não há favoritos', () => {
+    renderWithRouter(<App />);
+
+    const favoritesLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favoritesLink);
+
+    const noFavoritesMessage = screen.getByText('No favorite pokemon found');
+    expect(noFavoritesMessage).toBeInTheDocument();
+  });
+
+  test('b) Se são exibidos todos os cards de pokémons favoritados.', () => {
+    renderWithRouter(<App />);
+
+    const pokemonDetails = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(pokemonDetails);
+
+    const favoriteCheckbox = screen
+      .getByRole('checkbox', { name: 'Pokémon favoritado?' });
+    userEvent.click(favoriteCheckbox);
+
+    const favoritesLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(favoritesLink);
+
+    const favoriteCard = screen.getByRole('link', { name: 'More details' });
+    expect(favoriteCard).toBeInTheDocument();
+  });
+});
