a398aaeaec56269f7155f1f8daf290766b20f0f2
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-07 13:57:04 -0400

    Requisito 5 - Faz a abstração do teste do componente Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../uteis/renderWithRouter';
 import App from '../App';
+import Pokemons from '../data';
 
 describe('Testa o component Pokedex', () => {
   test('se página contém um heading h2 com o texto "Encountered pokémons".', () => {
@@ -9,5 +11,21 @@ describe('Testa o component Pokedex', () => {
     const titlePokedex = screen.getByRole('heading',
       { level: 2, name: 'Encountered pokémons' });
     expect(titlePokedex).toBeInTheDocument();
+  });
+
+  test(`se é exibido o próximo Pokémon da lista quando o
+  "botão Próximo pokémon" é clicado.`, () => {
+    renderWithRouter(<App />);
+    const buttonNextPokemon = screen.getByRole('button', { name: 'Próximo pokémon' });
+    userEvent.click(buttonNextPokemon);
+    expect(buttonNextPokemon).toBeInTheDocument();
+    // Passo a passo para os proximos testes
+    // 1.Pegar a lista de pokemons
+    Pokemons.forEach((pokemon) => {
+      console.log(pokemon.name);
+    });
+    // 2.Para cada pokemon da lista clicar no botão proximo botão
+    // 3. Após cada click verificar se o pokemon atual aparece na tela
+    // 4. Testar de existe mais de um role link com o nome "More details" na tela
   });
 });
