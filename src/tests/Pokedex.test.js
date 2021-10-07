5858e67e41b3405ab5cf6705c613c4386657ff54
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-07 10:05:49 -0400

    Requisito 5 - Testa se a pokedex tem um título na tela

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../uteis/renderWithRouter';
+import App from '../App';
+
+describe('Testa o component Pokedex', () => {
+  test('se página contém um heading h2 com o texto "Encountered pokémons".', () => {
+    renderWithRouter(<App />);
+    const titlePokedex = screen.getByRole('heading',
+      { level: 2, name: 'Encountered pokémons' });
+    expect(titlePokedex).toBeInTheDocument();
+  });
+});
