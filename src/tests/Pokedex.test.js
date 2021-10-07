bec118fdeb66e90bd6421766c3d733da9621437a
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-07 14:07:06 -0400

    Comenta o código anterior para corrigir erro no lint

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -3,7 +3,7 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../uteis/renderWithRouter';
 import App from '../App';
-import Pokemons from '../data';
+// import Pokemons from '../data';
 
 describe('Testa o component Pokedex', () => {
   test('se página contém um heading h2 com o texto "Encountered pokémons".', () => {
@@ -19,13 +19,13 @@ describe('Testa o component Pokedex', () => {
     const buttonNextPokemon = screen.getByRole('button', { name: 'Próximo pokémon' });
     userEvent.click(buttonNextPokemon);
     expect(buttonNextPokemon).toBeInTheDocument();
-    // Passo a passo para os proximos testes
-    // 1.Pegar a lista de pokemons
-    Pokemons.forEach((pokemon) => {
-      console.log(pokemon.name);
-    });
-    // 2.Para cada pokemon da lista clicar no botão proximo botão
-    // 3. Após cada click verificar se o pokemon atual aparece na tela
-    // 4. Testar de existe mais de um role link com o nome "More details" na tela
+    // // Passo a passo para os proximos testes
+    // // 1.Pegar a lista de pokemons
+    // Pokemons.forEach((pokemon) => {
+    //   console.log(pokemon.name);
+    // });
+    // // 2.Para cada pokemon da lista clicar no botão proximo botão
+    // // 3. Após cada click verificar se o pokemon atual aparece na tela
+    // // 4. Testar de existe mais de um role link com o nome "More details" na tela
   });
 });
