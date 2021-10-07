fa1543c08eb063703fcd1f28fb9707622863cff1
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-06 23:21:44 -0400

    Requisito 4 - Inicia os testes do componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../uteis/renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente "NotFound', () => {
+  test('se página contém um heading h2 com o texto "Page requested not found".', () => {
+    renderWithRouter(<NotFound />);
+    const title = screen.getByRole('heading', { level: 2,
+    });
+    expect(title).toBeInTheDocument();
+  });
+});
