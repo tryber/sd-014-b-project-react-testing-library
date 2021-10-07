fbda75c6dc6134b1929a89c9dacefe98ff7d520d
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-07 08:52:12 -0400

    Requisito 4 - Testa o título e a imagem do componente 'NotFound'

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -7,8 +7,14 @@ import NotFound from '../components/NotFound';
 describe('Testa o componente "NotFound', () => {
   test('se página contém um heading h2 com o texto "Page requested not found".', () => {
     renderWithRouter(<NotFound />);
-    const title = screen.getByRole('heading', { level: 2,
-    });
-    expect(title).toBeInTheDocument();
+    const title = screen.getByRole('heading', { level: 2 });
+    expect(title).toHaveTextContent('Page requested not found');
+  });
+
+  test('se página mostra a imagem "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif".', () => {
+    renderWithRouter(<NotFound />);
+    const imgNotFound = screen.getByRole('img',
+      { name: 'Pikachu crying because the page requested was not found' });
+    expect(imgNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
   });
 });
