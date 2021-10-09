f29c8ee6f091f1aec9559fc3addb02da9014f640
Patrick Paredes <patrickparedes44@gmail.com>
2021-10-08 22:46:58 -0400

    Requisito2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import About from '../components/About';
+
+describe('Testa se a página contém as informações da Pokédex', () => {
+  test('Teste se o heading contém o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const heading = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const paragraph = screen.getAllByText(/Pokémons/i);
+    expect(paragraph.length).toBe(2);
+  });
+
+  test('Teste se a página contém a imagem de uma Pokédex', () => {
+    renderWithRouter(<About />);
+    const image = screen.getByRole('img');
+    expect(image).toHaveAtributr('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
