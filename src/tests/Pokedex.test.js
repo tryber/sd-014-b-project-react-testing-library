f96eeea3bfce26e5bda57f0f6a9ed833a4d19358
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-10 19:17:39 -0400

    Requisito 5 - testa se cada pokemon é exibido na tela apos o click no botão proximo

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -3,7 +3,7 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../uteis/renderWithRouter';
 import App from '../App';
-// import Pokemons from '../data';
+import Pokemons from '../data';
 
 describe('Testa o component Pokedex', () => {
   test('se página contém um heading h2 com o texto "Encountered pokémons".', () => {
@@ -13,19 +13,25 @@ describe('Testa o component Pokedex', () => {
     expect(titlePokedex).toBeInTheDocument();
   });
 
-  test(`se é exibido o próximo Pokémon da lista quando o
-  "botão Próximo pokémon" é clicado.`, () => {
+  test('se existe um bottão com o texto "Próximo pokémon".', () => {
     renderWithRouter(<App />);
     const buttonNextPokemon = screen.getByRole('button', { name: 'Próximo pokémon' });
-    userEvent.click(buttonNextPokemon);
     expect(buttonNextPokemon).toBeInTheDocument();
-    // // Passo a passo para os proximos testes
-    // // 1.Pegar a lista de pokemons
-    // Pokemons.forEach((pokemon) => {
-    //   console.log(pokemon.name);
-    // });
-    // // 2.Para cada pokemon da lista clicar no botão proximo botão
-    // // 3. Após cada click verificar se o pokemon atual aparece na tela
-    // // 4. Testar de existe mais de um role link com o nome "More details" na tela
+  });
+
+  test(`se os próximos Pokémons da lista aparecem,
+  um a um, ao clicar sucessivamente no botão`, () => {
+    renderWithRouter(<App />);
+    const buttonNextPokemon = screen.getByRole('button', { name: 'Próximo pokémon' });
+    expect(buttonNextPokemon).toBeInTheDocument();
+    // Passo a passo para os proximos testes
+    // 1.Pegar a lista de pokemons
+    Pokemons.forEach((pokemon) => {
+      const namePokemon = screen.getByTestId('pokemon-name');
+      // 3. Após cada click verificar se o pokemon atual aparece na tela
+      expect(namePokemon).toHaveTextContent(pokemon.name);
+      // 2.Para cada pokemon da lista clicar no botão proximo botão
+      userEvent.click(buttonNextPokemon);
+    });
   });
 });
