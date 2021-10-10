eb7af04412c6890963df7bf5c15adb850c195ceb
Vanessa <vanessamrios@gmail.com>
2021-10-10 13:12:24 +0200

    (Req 01) Testa o componente App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,55 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  it('deveria possuir o texto Home', () => {
+    renderWithRouter(<App />);
+    const linkHome = screen.getByText(/Home/i);
+    expect(linkHome).toBeInTheDocument();
+  });
+  it('deveria possuir o texto About', () => {
+    renderWithRouter(<App />);
+    const linkAbout = screen.getByText(/About/i);
+    expect(linkAbout).toBeInTheDocument();
+  });
+  it('deveria possuir o texto Favorite Pokémons', () => {
+    renderWithRouter(<App />);
+    const linkFavorite = screen.getByText(/Favorite Pokémons/i);
+    expect(linkFavorite).toBeInTheDocument();
+  });
+});
+
+describe('se a aplicação é redirecionada para a página', () => {
+  it('deveria ser redirecionada para a página inicial ao clicar no link Home', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkHome = screen.getByText(/Home/i);
+    userEvent.click(linkHome);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+  it('deveria ser redirecionada para a página de About ao clicar no link About', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkAbout = screen.getByText(/About/i);
+    userEvent.click(linkAbout);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+  it('deveria ser redirecionad para a página Favorite ao clicar no link Favorite', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkFavorite = screen.getByText(/Favorite Pokémons/i);
+    userEvent.click(linkFavorite);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+  it('redireciona para a página Not Found ao entrar em uma URL desconhecida', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/pagina/que-nao-existe/');
+
+    const notFoundTitle = screen.getByText(/Page requested not found/i);
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+});
