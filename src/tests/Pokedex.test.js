885e057154868a09e89ff229797ecac3cd176564
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-10-10 20:22:28 -0300

    verificando a existencia de um botão proximo pokemon e se o proximo pokemon (charmander) da lista é renderizado, tive dificuldade em renderizar o componente certo,

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,16 +1,29 @@
-it('', () => {});
-// import React from 'react';
-// import { screen } from '@testing-library/react';
-// import { Pokedex } from '../components';
-// import renderWithRouter from './renderWithRouter';
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import Pokedex from '../components/Pokedex';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+import userEvent from '@testing-library/user-event';
+// import pokemons from '../data';
 
-// describe(' Teste o componente <Pokedex.js />', () => {
-//   it('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
-//     renderWithRouter(<Pokedex />);
-//     const heading = screen.getByRole('heading', {
-//       level: 2,
-//       name: 'Encountered pokémons',
-//     });
-//     expect(heading).toBeInTheDocument();
-//   });
-// });
+describe(' Teste o componente <Pokedex.js />', () => {
+  it('Teste se página contém um heading h2 com o texto Encountered pokémons',
+    () => {
+      renderWithRouter(<App />);
+      const heading = screen.getByRole('heading', {
+        level: 2,
+        name: 'Encountered pokémons',
+      });
+      expect(heading).toBeInTheDocument();
+    });
+
+  it('Se o próximo pokemon é o Charmander', () => {
+    renderWithRouter(<App />);
+    const nextButton = screen.getByRole('button', {
+      name: 'Próximo pokémon',
+    });
+    expect(nextButton).toBeInTheDocument();
+    userEvent.click(nextButton);
+    expect(screen.getByText(/charmander/i)).toBeInTheDocument();
+  });
+});
