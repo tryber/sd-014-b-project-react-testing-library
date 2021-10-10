bf7d9bb01cbf8a3c230777eb08d374174c4b4f8f
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-10-10 14:22:28 -0300

    testa se contem h2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import RenderWithRouter from '../RenderWithRouter';
+import About from '../components/About';
+
+describe('Teste se a página contém as informações sobre a Pokédex.', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    RenderWithRouter(<About />);
+    const aboutHeading = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(aboutHeading).toBeInTheDocument();
+  });
+});
