d074f56452f192da4cfbfb1a037443363e9b24bd
Vanessa <vanessamrios@gmail.com>
2021-10-10 19:13:56 +0200

    (Req 04) Testa o componente NotFound

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,9 @@
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// import renderWithRouter from './renderWithRouter';
+// import { FavoritePokemons } from '../components';
+
+// describe('Testa o componente FavoritePokemons', () => {
+//   test('', () => {});
+// });
 test('', () => {});
