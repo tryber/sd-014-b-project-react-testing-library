2d5f6d2e340cad2d2c5459f34c506bd2069b2d34
GustavoPD <gudalmoro@gmail.com>
2021-10-11 00:17:42 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -10,7 +10,6 @@ describe('Se são renderizadas as informações sobre a Pokedex', () => {
       level: 2,
       name: 'About Pokédex',
     });
-
     expect(aboutHeading).toBeInTheDocument();
   });
 
@@ -19,8 +18,15 @@ describe('Se são renderizadas as informações sobre a Pokedex', () => {
 
     const aboutInfosOne = screen.getByText(/simulates/i);
     const aboutInfosTwo = screen.getByText(/filter/i);
-
     expect(aboutInfosOne).toBeInTheDocument();
     expect(aboutInfosTwo).toBeInTheDocument();
+  });
+
+  test('Se a página contém a seguinte imagem de uma Pokédex', () => {
+    render(<About />);
+
+    const pokedexImg = screen.getByRole('img');
+    expect(pokedexImg).toHaveAttribute('src',
+      'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
   });
 });
