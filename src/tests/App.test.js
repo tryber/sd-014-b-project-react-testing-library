b5a0a2a8bf13f2b56eaf2f416ac08d55103526f9
dsomarcelo <celoygm@gmail.com>
2021-10-10 22:18:04 -0300

    Teste do primeiro requisito e monstagem do renderWithRouter

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,2 +1,15 @@
-test('', () => {});
-// Comentario para o primeiro push
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+test('Se existem links na página inicial', () => {
+  renderWithRouter(<App />);
+  const linkHome = screen.getByRole('link', { name: 'Home' });
+  const linkAbout = screen.getByRole('link', { name: 'About' });
+  const linkFav = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+  expect(linkHome).toBeInTheDocument();
+  expect(linkAbout).toBeInTheDocument();
+  expect(linkFav).toBeInTheDocument();
+});
