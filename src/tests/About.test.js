49c736f2a7735e0658ec90db30fa5cf0b0025edd
GustavoDamm <gustavo.damm15@hotmail.com>
2021-10-11 13:21:41 -0300

    about

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import About from '../components/About';
 import renderWithRouter from './renderWithRouter';
 
