89c06c2fbb8a2d05e515d16ef02637e681beda3c
dsomarcelo <celoygm@gmail.com>
2021-10-11 11:21:38 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { About } from '../components';
+
+test('Se a página contém as informações sobre a Pokédex.', () => {
+  render(<About />);
+
+  const h2 = screen.getByRole('heading', { level: 2 });
+
+  expect(h2).toBeInTheDocument();
+  expect(h2).toHaveTextContent('About Pokédex');
+
+  const p1 = screen.getByText('This application simulates a Pokédex, a '
+    + 'digital encyclopedia containing all Pokémons');
+  const p2 = screen.getByText('This application simulates a Pokédex, a '
+    + 'digital encyclopedia containing all Pokémons');
+
+  expect(p1).toBeInTheDocument();
+  expect(p2).toBeInTheDocument();
+
+  const img = screen.getByRole('img');
+  expect(img).toHaveAttribute(
+    'src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/'
+      + '800px-Gen_I_Pok%C3%A9dex.png',
+  );
+});
