0e1250eb80a4b2537cd8d9e5ab24c667f2cfb8c8
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-10-11 09:33:26 -0300

    testando se o nome e o tipo do pokemon corresponde ao que está na tela

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+// import pokemons from '../data';
+
+describe('Teste o componente <Pokemon.js />', () => {
+  it('O nome correto do Pokémon deve ser mostrado na tela', () => {
+    renderWithRouter(<App />);
+    const pokemonName = screen.getByTestId('pokemon-name');
+    expect(pokemonName.innerHTML).toBe('Pikachu');
+  });
+
+  it('O tipo correto do pokémon deve ser mostrado na tela', () => {
+    renderWithRouter(<App />);
+    const pokemonType = screen.getByTestId('pokemon-type');
+    expect(pokemonType.innerHTML).toBe('Electric');
+  });
+
+});
