99f52fea91f0732dcfdf4eaee06400a0e50bbb21
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-10-11 11:15:08 -0300

    comentando as importações que eu ainda vou utilizar do requisito 7 para não dar erro de Linter

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import renderWithRouter from './renderWithRouter';
-import App from '../App';
+// import React from 'react';
+// import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import renderWithRouter from './renderWithRouter';
+// import App from '../App';
 
 describe('Teste o componente <PokemonDetails.js />', () => {
   it('Teste se as informações detalhadas do Pokémon selecionado são mostradas na tela',
