c0f028f30132079030a9debe29ed041d058e0876
daniel <daniel.acoelho@outlook.com>
2021-10-11 09:32:59 -0300

    Requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './RenderWithRouter';
+
+describe('Verifica o componente Pokemon', () => {
+  test('Verifica se é renderizado um card com as informações de determinado pokémon',
+    () => {
+      renderWithRouter(<App />);
+      const pikachuImage = screen.getByRole('img');
+      expect(pikachuImage).toHaveAttribute('src',
+        'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    });
+});
