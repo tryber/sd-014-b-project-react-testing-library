cb8f209c298fbe2fa92ad6ccdd6f438d96647ea8
s4bot4 <sabotamail@gmail.com>
2021-10-11 08:33:33 -0300

    Fix lint. Req. 01

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,2 +1,13 @@
-test('', () => {});
- 
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../RenderWithRouter';
+import { About } from '../components';
+
+describe('Teste se a página contém as informações sobre a Pokédex.', () => {
+  test('Se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+    const aboutTitle = screen.getByRole('heading',
+      { name: 'About Pokédex' });
+    expect(aboutTitle).toBeInTheDocument();
+  });
+});
