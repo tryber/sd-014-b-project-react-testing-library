0e1eb1844e24399a78da6099cd4304abd8975302
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-10-11 09:41:10 -0300

    verificando se o texto de peso e medida do peso corresponde com a do pokemon verificando o InnerHTML do elemento!

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -10,11 +10,20 @@ describe('Teste o componente <Pokemon.js />', () => {
     const pokemonName = screen.getByTestId('pokemon-name');
     expect(pokemonName.innerHTML).toBe('Pikachu');
   });
-
   it('O tipo correto do pokémon deve ser mostrado na tela', () => {
     renderWithRouter(<App />);
     const pokemonType = screen.getByTestId('pokemon-type');
     expect(pokemonType.innerHTML).toBe('Electric');
+  });
+
+  it(`O peso médio do pokémon deve ser exibido com um texto
+   no formato Average weight: <value> <measurementUnit>;
+   onde <value> e <measurementUnit> são, respectivamente,
+   o peso médio do pokémon e sua unidade de medida`,
+  () => {
+    renderWithRouter(<App />);
+    const pokemonType = screen.getByTestId('pokemon-weight');
+    expect(pokemonType.innerHTML).toBe('Average weight: 6.0 kg');
   });
 
 });
