581f6c99334482d196764438300ab5ce20605091
Thiago Gomez <thiagogomez2906@hotmail.com>
2021-10-11 14:01:36 -0300

    lint

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../RenderwithRouter';
-import Pokedex from '../components/Pokedex';
 import App from '../App';
 import pokemons from '../data';
 
