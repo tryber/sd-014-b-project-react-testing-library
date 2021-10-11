e67780a048e711c71cb707f10ab7ee0c302072a1
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-10-11 10:07:33 -0300

    verificando se tem um link com o atributo href direcionando para o link do pokemons/25

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -17,4 +17,15 @@ describe('Teste o componente <Pokemon.js />', () => {
     expect(pokemonImg).toHaveAttribute('src',
       'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
   });
+
+  it(`Teste se o card do Pokémon indicado na Pokédex contém um link de navegação
+  para exibir detalhes deste Pokémon. O link deve possuir a URL /pokemons/<id>,
+  onde <id> é o id do Pokémon exibido`, () => {
+    renderWithRouter(<App />);
+    const link = screen.getByRole('link', {
+      name: 'More details',
+    });
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveAttribute('href', '/pokemons/25');
+  });
 });
