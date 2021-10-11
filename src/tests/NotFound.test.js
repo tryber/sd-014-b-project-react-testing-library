ae8bd53609a185c3722a7ac2825ba029403f8c7e
Ana-Alice-Honorio <ana.alice.honorio@gmail.com>
2021-10-11 19:42:52 -0300

    req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import { NotFound } from '../components';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('contém um heading h2 com o texto "Page requested not found 😭"', () => {
+    render(<NotFound />);
+    const imageParagraph = screen.getByRole('heading', { level: 2 });
+    expect(imageParagraph.textContent).toBe('Page requested not found 😭');
+  });
+
+  it('Teste se página mostra a imagem', () => {
+    render(<NotFound />);
+    const imageNotFound = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(imageNotFound.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
