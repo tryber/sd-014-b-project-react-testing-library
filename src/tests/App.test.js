f190d25dc9db9ed7a7a50f823b22263f748f4ee2
GustavoPD <gudalmoro@gmail.com>
2021-10-10 23:14:10 -0300

    Req 1 done

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
 
@@ -19,6 +20,39 @@ describe('Testa a aplicação App', () => {
     expect(homeLink).toBeInTheDocument();
     expect(aboutLink).toBeInTheDocument();
     expect(favPokeLink).toBeInTheDocument();
+  });
+
+  test('Se a página é redirecionada para / quando clica em Home', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', {
+      name: 'Home',
+    });
+
+    userEvent.click(homeLink);
+    expect(history.location.pathname).toBe('/');
+  });
+
+  test('Se a página é redirecionada para /about pelo link About', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const aboutLink = screen.getByRole('link', {
+      name: 'About',
+    });
+
+    userEvent.click(aboutLink);
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  test('Se a página é redirecionada para /favorites pelo link Favorites', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const favPokeLink = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+
+    userEvent.click(favPokeLink);
+    expect(history.location.pathname).toBe('/favorites');
   });
 
   test('Se é direcionada para a página Not Found ao entrar em URL desconhecida', () => {
