f2639664117efc5118fc1fb87c0fe49d1455759b
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-11 21:18:12 -0400

    Requisito 6 - testa se o tipo correto do pokemon aparece na tela

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -6,25 +6,23 @@ import App from '../App';
 import pokemons from '../data';
 
 describe('Testa o componente "Pokemon".', () => {
-  test('se o nome do pokemon correto aparece na tela', () => {
-    // renderizar o app
+  test('se o nome correto do pokemon aparece na tela', () => {
     renderWithRouter(<App />);
-    // capturar a lista de tipos
     pokemons.forEach((pokemon) => {
-      // verificar se o nome do pokemon na tela é igual ao nome do 1 pokemon na lista
       const pokemonName = screen.getByTestId('pokemon-name');
       expect(pokemonName.innerHTML).toBe(pokemon.name);
-
-      console.log(`pokemon na tela ${pokemonName.innerHTML}`);
-
-      console.log(`pokemon na lista ${pokemon.name}`);
-
-      console.log(`Data pokemon ${pokemon.type}`);
-
-      // // para cada tipo clicar no botão proximo
       const buttonNextPokemon = screen.getByRole('button', { name: 'Próximo pokémon' });
       userEvent.click(buttonNextPokemon);
     });
-    // após o click verificar se o nome correto estã la tela
+  });
+
+  test('se o tipo correto do pokemon aparece na tela', () => {
+    renderWithRouter(<App />);
+    pokemons.forEach((pokemon) => {
+      const pokemonName = screen.getByTestId('pokemon-type');
+      expect(pokemonName.innerHTML).toBe(pokemon.type);
+      const buttonNextPokemon = screen.getByRole('button', { name: 'Próximo pokémon' });
+      userEvent.click(buttonNextPokemon);
+    });
   });
 });
