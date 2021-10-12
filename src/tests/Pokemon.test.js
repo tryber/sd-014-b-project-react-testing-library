7fb9c4784c44cebb00df9de33e1b37e84404a73f
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-11 21:14:05 -0400

    Requisito 6 - testa se o nome corretos dos pokemons aparecem na tela

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../uteis/renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Testa o componente "Pokemon".', () => {
+  test('se o nome do pokemon correto aparece na tela', () => {
+    // renderizar o app
+    renderWithRouter(<App />);
+    // capturar a lista de tipos
+    pokemons.forEach((pokemon) => {
+      // verificar se o nome do pokemon na tela é igual ao nome do 1 pokemon na lista
+      const pokemonName = screen.getByTestId('pokemon-name');
+      expect(pokemonName.innerHTML).toBe(pokemon.name);
+
+      console.log(`pokemon na tela ${pokemonName.innerHTML}`);
+
+      console.log(`pokemon na lista ${pokemon.name}`);
+
+      console.log(`Data pokemon ${pokemon.type}`);
+
+      // // para cada tipo clicar no botão proximo
+      const buttonNextPokemon = screen.getByRole('button', { name: 'Próximo pokémon' });
+      userEvent.click(buttonNextPokemon);
+    });
+    // após o click verificar se o nome correto estã la tela
+  });
+});
