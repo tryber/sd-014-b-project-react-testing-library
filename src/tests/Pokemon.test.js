7aa062406fd628a2d28dca576df9534eb75b9fe3
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-11 23:37:10 -0400

    Requisito 6 - [REFATORADO] testa se a imagem do pokemon aparece na tela

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -6,44 +6,45 @@ import App from '../App';
 import pokemons from '../data';
 
 describe('Testa o componente "Pokemon".', () => {
-  test('se o nome correto do pokemon aparece na tela', () => {
+  const NEXT_POKEMON = 'Próximo pokémon';
+  beforeEach(() => {
     renderWithRouter(<App />);
+  });
+
+  test('se o nome correto do pokemon aparece na tela', () => {
     pokemons.forEach((pokemon) => {
       const pokemonName = screen.getByTestId('pokemon-name');
       expect(pokemonName.innerHTML).toBe(pokemon.name);
-      const buttonNextPokemon = screen.getByRole('button', { name: 'Próximo pokémon' });
+      const buttonNextPokemon = screen.getByRole('button', { name: NEXT_POKEMON });
       userEvent.click(buttonNextPokemon);
     });
   });
 
   test('se o tipo correto do pokemon aparece na tela', () => {
-    renderWithRouter(<App />);
     pokemons.forEach((pokemon) => {
       const pokemonName = screen.getByTestId('pokemon-type');
       expect(pokemonName.innerHTML).toBe(pokemon.type);
-      const buttonNextPokemon = screen.getByRole('button', { name: 'Próximo pokémon' });
+      const buttonNextPokemon = screen.getByRole('button', { name: NEXT_POKEMON });
       userEvent.click(buttonNextPokemon);
     });
   });
 
   test('se o peso médio correto do pokemon aparece na tela', () => {
-    renderWithRouter(<App />);
     pokemons.forEach((pokemon) => {
       const pokemonName = screen.getByTestId('pokemon-weight');
       expect(pokemonName.innerHTML).toBe(`Average weight: ${
         pokemon.averageWeight.value} ${pokemon.averageWeight.measurementUnit}`);
-      const buttonNextPokemon = screen.getByRole('button', { name: 'Próximo pokémon' });
+      const buttonNextPokemon = screen.getByRole('button', { name: NEXT_POKEMON });
       userEvent.click(buttonNextPokemon);
     });
   });
 
   test('se a imagem do pokemon é exibida corretamente', () => {
-    renderWithRouter(<App />);
     pokemons.forEach((pokemon) => {
       const imgPokemon = screen.getByRole('img', { name: `${pokemon.name} sprite` });
       expect(imgPokemon).toBeInTheDocument();
       expect(imgPokemon.src).toStrictEqual(pokemon.image);
-      const buttonNextPokemon = screen.getByRole('button', { name: 'Próximo pokémon' });
+      const buttonNextPokemon = screen.getByRole('button', { name: NEXT_POKEMON });
       userEvent.click(buttonNextPokemon);
     });
   });
