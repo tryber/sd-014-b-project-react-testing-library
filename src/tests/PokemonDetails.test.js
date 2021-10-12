e6d8adbc980d39b75f6d912ebb2e65a270bd5e7c
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-12 00:50:18 -0400

    Requisito 7 - testa se o título de detalhes do pokemon aparece

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../uteis/renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Testa o componente "PokemonDetails".', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+
+  test('se o título de detalhes do pokemon está na tela', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const h2PokemonDetails = screen
+      .getByRole('heading', { name: `${pokemons[0].name} Details` });
+    expect(h2PokemonDetails).toBeInTheDocument();
+  });
+});
