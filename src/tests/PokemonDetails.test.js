e02e5af31acd5fad426d96ae9b1395eac5cbba23
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-10-12 22:11:35 -0400

    Requisito 7 - testa se o título dos locais do pokemon e os mapa aparece na tela

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -16,4 +16,28 @@ describe('Testa o componente "PokemonDetails".', () => {
       .getByRole('heading', { name: `${pokemons[0].name} Details` });
     expect(h2PokemonDetails).toBeInTheDocument();
   });
+
+  test('se o H2 com o texto "Summary" está na tela', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const summaryText = screen.getByRole('heading', { level: 2, name: 'Summary' });
+    expect(summaryText).toBeInTheDocument();
+  });
+
+  test(`se existi um h2 com o texto "Game Locations of <name>;
+  onde <name> é o nome do Pokémon exibido.`, () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const h2GameLocations = screen.getByRole('heading',
+      { level: 2, name: 'Game Locations of Pikachu' });
+    expect(h2GameLocations).toBeInTheDocument();
+  });
+
+  test('Se a imagem do local do pokemon está na tela', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const imgMap = screen.getAllByRole('img', { name: 'Pikachu location' });
+    expect(imgMap[0]).toBeInTheDocument();
+    expect(imgMap[0].src).toBe('https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+  });
 });
