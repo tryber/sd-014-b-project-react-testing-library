7ef93ff3f372401406f1704e47037795b785f3de
Patrick Paredes <patrickparedes44@gmail.com>
2021-10-13 00:39:16 -0400

    requisito3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import { FavoritePokemons } from '../components';
-import renderWithRouter from '../renderWithRouter';
+import renderWithRouter from './renderWithRouter';
 
 describe('Testa o componente <FavoritePokemons.js />', () => {
   test('Testa se é exibido na tela a mensagem `No favorite pokemon found`, se a pessoa não tiver pokémons favoritos.', () => {
     renderWithRouter(<FavoritePokemons />);
-    const mensage = screen.getByText('No favorite pokemon found');
+    const mensage = screen.getByText('/No favorite pokemon found/i');
     expect(mensage).toBeInTheDocument();
   });
 
