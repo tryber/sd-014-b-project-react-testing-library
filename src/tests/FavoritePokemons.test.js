a2f22ddfd0d1450cd478cc499a043a5dd59bfba9
Patrick Paredes <patrickparedes44@gmail.com>
2021-10-12 23:41:17 -0400

    stryker

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente <FavoritePokemons.js />', () => {
+  test('Testa se é exibido na tela a mensagem `No favorite pokemon found`, se a pessoa não tiver pokémons favoritos.', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const mensage = screen.getByText('No favorite pokemon found');
+    expect(mensage).toBeInTheDocument();
+  });
+});
