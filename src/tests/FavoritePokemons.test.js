8ca450ab9316b4201b587e779485a5b337c79546
dhenycruz <dheniarley.ds@gmail.com>
2021-10-16 17:22:10 -0300

    Requisito 3.

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente <FavoritePokemons.js />', () => {
+  beforeEach(() => {
+    localStorage.setItem('favoritePokemonIds', JSON.stringify([]));
+  });
+  afterEach(() => localStorage.clear());
+
+  const mensagem = 'No favorite pokemon found';
+  test(`Testa se é exibido a mensagem ${mensagem}, se não tiver pokémons favoritos.`,
+    () => {
+      renderWithRouter('/favorites');
+      const pokemonsFavorites = JSON.parse(localStorage.getItem('favoritePokemonIds'));
+      const text = screen.getByText(mensagem);
+
+      expect(pokemonsFavorites.length).toBe(0);
+      expect(text).toBeInTheDocument();
+    });
+});
