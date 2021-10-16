42ff5797867450a762b952ff05a10222fa61fb56
= <luz.matheus11@gmail.com>
2021-10-16 16:25:35 -0300

    Requisito 3 e 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Requisito 3 - Teste o componente FavoritePokemons', () => {
+  test('se é exibido na tela a mensagem ` No favorite pokemon found`', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const notFound = screen.getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+});
