88420d283f4236885fc19c4d02c905442f58b1cb
Sergio_Rodrigues <rodrigues.sergiofilho@gmail.com>
2021-10-16 21:13:58 -0300

    Implementa os testes para o Pokemon (Req6)

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,50 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+
+describe('Testa se o card do pokemon contem as informações do respectivo pokemon', () => {
+  it('testa se contem o nome do pokemon', () => {
+    renderWithRouter(<App />);
+    const pokemonName = screen.getByTestId('pokemon-name');
+    expect(pokemonName.textContent).toBe('Pikachu');
+  });
+
+  it('Testa se card possui o tipo do pokemon', () => {
+    renderWithRouter(<App />);
+    const pokemonType = screen.getByTestId('pokemon-type');
+    expect(pokemonType.textContent).toBe('Electric');
+  });
+
+  it('Testa se o card possui o peso do pokemon', () => {
+    renderWithRouter(<App />);
+    const pokemonWeight = screen.getByTestId('pokemon-weight');
+    expect(pokemonWeight.textContent).toBe('Average weight: 6.0 kg');
+  });
+
+  it('Testa se o card possui a imagem do pokemon', () => {
+    renderWithRouter(<App />);
+    const img = screen.getByAltText('Pikachu sprite');
+    expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+
+  it('Testa se More details encaminha para a url esperada', () => {
+    const { history } = renderWithRouter(<App />);
+    const moreDetails = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(moreDetails);
+    expect(history.location.pathname).toBe('/pokemons/25');
+  });
+
+  it('Testa se o incone de favorito funciona como esperado', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const checkbox = screen.getByRole('checkbox', { name: 'Pokémon favoritado?' });
+    expect(checkbox).toBeInTheDocument();
+    userEvent.click(checkbox);
+    expect(checkbox).toBeChecked();
+    const star = screen.getByAltText('Pikachu is marked as favorite');
+    expect(star).toBeInTheDocument();
+    expect(star).toHaveAttribute('src', '/star-icon.svg');
+  });
+});
