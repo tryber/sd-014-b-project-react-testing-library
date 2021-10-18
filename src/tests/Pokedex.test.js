fd77e4c949abe65bcc4c243982eb2735dcac2877
= <luz.matheus11@gmail.com>
2021-10-18 08:49:30 -0300

    Requisito 6

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import { Pokedex } from '../components';
+
+describe('Requisito 5 - Teste o componente Pokedex', () => {
+  test('se a página contém um h2 com o texto Encountered pokémons ', () => {
+    renderWithRouter(<App />);
+
+    const subtitle = screen.getByRole('heading',
+      { level: 2, name: 'Encountered pokémons' });
+
+    expect(subtitle).toBeInTheDocument();
+  });
+
+  test('se é exibido o próximo pokémon da lista quando é clicado', () => {
+    renderWithRouter(<Pokedex />);
+  });
+});
