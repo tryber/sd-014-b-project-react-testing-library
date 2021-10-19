37a6687654107d29f6ef85cc24ea98f8f577fb95
Tiago Ponce <tiagocponce@gmail.com>
2021-10-19 17:39:02 -0300

    Fix lint

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,20 +3,12 @@ import { render, screen } from '@testing-library/react';
 import About from '../components/About';
 
 test('Se a pagina contem um heading h2 com o texto About "About Pokédex" ', () => {
-  render (<About />);
+  render(<About />);
 
   const title = screen.getByRole('heading', {
     level: 2,
     name: 'About Pokédex',
-  })
+  });
 
   expect(title).toBeInTheDocument();
 });
-
-test('Se a pagina contem dois paragrafos com texto sobre a Pokedex', () => {
-  render (<About />);
-
-  const paragraphs = screen.getAllByRole('p');
-
-  expect(paragraphs).toHaveLength(2);
-})
