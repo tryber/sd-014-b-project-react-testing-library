f021c31ace8403ad355622abdd735a1a35d7d9af
Tiago Ponce <tiagocponce@gmail.com>
2021-10-19 17:08:32 -0300

    Requirement 01

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+test('Se a pagina contem um heading h2 com o texto About "About Pokédex" ', () => {
+  render (<About />);
+
+  const title = screen.getByRole('heading', {
+    level: 2,
+    name: 'About Pokédex',
+  })
+
+  expect(title).toBeInTheDocument();
+});
+
+test('Se a pagina contem dois paragrafos com texto sobre a Pokedex', () => {
+  render (<About />);
+
+  const paragraphs = screen.getAllByRole('p');
+
+  expect(paragraphs).toHaveLength(2);
+})
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,76 @@
-test('se o primeiro link possui o texto Home', () => {});
+import React from 'react';
+import { render, screen } from "@testing-library/react";
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+import userEvent from '@testing-library/user-event';
+
+// test('Verifica se existem 3 links de navegacao', () => {
+//   const customHistory = createMemoryHistory();
+//   render(
+//     <Router history={customHistory}> 
+//       <App />
+//     </Router>
+//   );
+
+//   const links = screen.getAllByRole('link');
+
+//   expect(links).toHaveLength(4);
+//   expect(links[0]).toHaveValue('Home');
+//   expect(links[1]).toHaveValue('About');
+//   expect(links[2]).toHaveValue('Favorite Pokémons');
+// });
+
+test('Verifica se a aplicacao eh redirecionada para a pagina inicial ao clicar no Home', () => {
+  const customHistory = createMemoryHistory();
+  render
+  (
+    <Router history={customHistory}>
+      <App />
+    </Router>
+  );
+
+  const linkHome = screen.getByRole('link', { name: 'Home' });
+
+  userEvent.click(linkHome);
+
+
+  expect(customHistory.location.pathname).toBe('/');
+
+})
+
+test('Verifica se a aplicacao eh redirecionada para a pagina inicial ao clicar no Home', () => {
+  const customHistory = createMemoryHistory();
+  render
+  (
+    <Router history={customHistory}>
+      <App />
+    </Router>
+  );
+
+  const linkAbout = screen.getByRole('link', { name: 'About' });
+
+  userEvent.click(linkAbout);
+
+
+  expect(customHistory.location.pathname).toBe('/about');
+
+})
+
+test('Verifica se a aplicacao eh redirecionada para a pagina inicial ao clicar no Home', () => {
+  const customHistory = createMemoryHistory();
+  render
+  (
+    <Router history={customHistory}>
+      <App />
+    </Router>
+  );
+
+  const linkFavorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+  userEvent.click(linkFavorite);
+
+
+  expect(customHistory.location.pathname).toBe('/favorites');
+
+})
