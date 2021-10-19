daeff0ba1a0b63a2be2e82d3fbec40fcfaeaeb5d
larymanzo <lamosinha@hotmail.com>
2021-10-18 22:54:00 -0300

    Requisito 2

--- src/components/About.js
+++ src/components/About.js
@@ -2,15 +2,16 @@ import React from 'react';
 import './about.css';
 
 const About = () => (
-  <section>
-    <h2>{ `About ` + `Pokédex` }</h2>
-    <section>
-      <p>
+  <section  data-testid="about-pokedex">
+    <h2 data-testid="about-text-about">{ `About ` + `Pokédex` }</h2>
+    <section data-testid="about-content">
+      <p data-testid="about-paragraph">
         This application simulates a Pokédex, a
         digital encyclopedia containing all Pokémons
       </p>
-      <p>One can filter Pokémons by type, and see more details for each one of them</p>
+      <p data-testid="about-paragraph">One can filter Pokémons by type, and see more details for each one of them</p>
       <img
+        data-testid="about-image-pokedex"
         className="pokedex-image"
         src={ `https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png` }
         alt="Pokédex"
--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Página about', () => {
+  it('Testa se a página contém as informações sobre a Pokédex', () => {
+    render(<About />);
+    const aboutPokedex = screen.getByTestId('about-pokedex');
+    expect(aboutPokedex).toBeInTheDocument();
+  });
+
+  it('Testa se a página contém um heading h2 com o texto About Pokédex', () => {
+    render(<About />);
+    const aboutTextAbout = screen.getByTestId('about-text-about');
+    expect(aboutTextAbout).toHaveTextContent('About Pokédex');
+  });
+
+  it('Testa se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    render(<About />);
+    const aboutParagraph = screen.getAllByTestId('"about-paragraph"');
+    expect(aboutParagraph).toHaveLength(2);
+  });
+
+  it('Testa se a página contém a imagem de uma Pokédex', () => {
+    render(<About />);
+    const aboutImagePokedex = screen.getByTestId('about-image-pokedex');
+    expect(aboutImagePokedex.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
