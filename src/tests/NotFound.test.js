0c58261508915c2111461197889e46280316e0ad
rphsantos1995 <rph.santos@Outlook.com>
2021-10-20 11:10:00 -0300

    Undo alteration in notfound component, hopefully its gonna pass

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -9,9 +9,10 @@ describe('Requisito 4: Teste o componente <NotFound.js />', () => {
     const notFoundText = screen.getByText(/Page requested not found/i);
     expect(notFoundText).toBeInTheDocument();
   });
+
   test('mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
     renderWithRouter(<NotFound />);
-    const notFoundImg = screen.getByTestId('image-not-found');
+    const notFoundImg = screen.getByAltText('Pikachu crying because the page requested was not found');
     const notFoundSrc = notFoundImg.getAttribute('src');
     expect(notFoundImg).toBeInTheDocument();
     expect(notFoundSrc).toEqual('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+describe('Teste o componente <Pokemon.js />', () => {
+  test(`'Teste se é renderizado um card 
+  com as informações de determinado pokémon.'`, () => {
+
+  });
+  test(`'Teste se o card do Pokémon indicado 
+  na Pokédex contém um link de navegação'`, () => {
+
+  });
+  test(`'Teste se ao clicar no link de navegação do Pokémon, é 
+    feito o redirecionamento da aplicação para a página de detalhes de Pokémon'`, () => {
+
+  });
+  test(`'Teste também se a URL exibida no navegador muda para 
+  /pokemon/<id>, onde <id> é o id do Pokémon cujos detalhes se deseja ver'`, () => {
+
+  });
+});
