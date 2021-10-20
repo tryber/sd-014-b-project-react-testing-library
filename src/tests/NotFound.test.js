7884493b763779d1101ea1b70e9bdfabacadd55a
rphsantos1995 <rph.santos@Outlook.com>
2021-10-20 11:11:05 -0300

    ...

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -12,7 +12,8 @@ describe('Requisito 4: Teste o componente <NotFound.js />', () => {
 
   test('mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
     renderWithRouter(<NotFound />);
-    const notFoundImg = screen.getByAltText('Pikachu crying because the page requested was not found');
+    const notFoundImg = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
     const notFoundSrc = notFoundImg.getAttribute('src');
     expect(notFoundImg).toBeInTheDocument();
     expect(notFoundSrc).toEqual('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
