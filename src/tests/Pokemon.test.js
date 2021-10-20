04a994c9f37ea5b086aea55d2dd003510d01b143
rphsantos1995 <rph.santos@Outlook.com>
2021-10-20 11:43:53 -0300

    Req 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,18 +1,44 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
 describe('Teste o componente <Pokemon.js />', () => {
   test(`'Teste se é renderizado um card 
   com as informações de determinado pokémon.'`, () => {
+    renderWithRouter(<App />);
+    const pikachuButton = screen.getByRole('button', { name: 'Electric' });
+    userEvent.click(pikachuButton);
+    const name = screen.getByTestId('pokemon-name');
+    const type = screen.getByTestId('pokemon-type');
+    const weight = screen.getByTestId('pokemon-weight');
+    const image = screen.getByAltText('Pikachu sprite');
 
+    expect(name.textContent).toBe('Pikachu');
+    expect(type.textContent).toBe('Electric');
+    expect(weight.textContent).toBe('Average weight: 6.0 kg');
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
   });
   test(`'Teste se o card do Pokémon indicado 
   na Pokédex contém um link de navegação'`, () => {
-
+    const { history } = renderWithRouter(<App />);
+    const moreDetailsLink = screen.getByRole('link', { name: 'More details' });
+    expect(moreDetailsLink).toBeInTheDocument();
+    userEvent.click(moreDetailsLink);
+    expect(history.location.pathname).toBe('/pokemons/25');
   });
-  test(`'Teste se ao clicar no link de navegação do Pokémon, é 
-    feito o redirecionamento da aplicação para a página de detalhes de Pokémon'`, () => {
 
-  });
-  test(`'Teste também se a URL exibida no navegador muda para 
-  /pokemon/<id>, onde <id> é o id do Pokémon cujos detalhes se deseja ver'`, () => {
+  test('\'se existe um ícone de estrela nos Pokémons favoritados.\'', () => {
+    renderWithRouter(<App />);
 
+    const linkDetails = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(linkDetails);
+
+    const checkPokemon = screen.getByRole('checkbox');
+    userEvent.click(checkPokemon);
+
+    const favImgStar = screen.getByAltText('Pikachu is marked as favorite');
+    expect(favImgStar).toHaveAttribute('src', '/star-icon.svg');
   });
 });
