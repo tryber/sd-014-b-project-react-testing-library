dcd99d007184ffef16ac77135807084802a34a95
Tiago Ponce <tiagocponce@gmail.com>
2021-10-24 20:02:56 -0300

    Requirement 01 e 02 finish , fix lint

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -12,3 +12,27 @@ test('Se a pagina contem um heading h2 com o texto About "About Pokédex" ', () 
 
   expect(title).toBeInTheDocument();
 });
+
+test('Se a pagina contem 2 paragrafos com texto sobre a Pokedex', () => {
+  render(<About />);
+
+  const textFirstOne = 'This application simulates a Pokédex,';
+  const textFirstSecond = ' a digital encyclopedia containing all Pokémons';
+  const textAllParagraph = textFirstOne + textFirstSecond;
+  const textSecondOne = 'One can filter Pokémons by type,';
+  const textSecondTwo = ' and see more details for each one of them';
+  const textAllSecond = textSecondOne + textSecondTwo;
+  const firstParagraph = screen.getByText(textAllParagraph);
+  const secondParagraph = screen.getByText(textAllSecond);
+
+  expect(firstParagraph).toBeInTheDocument();
+  expect(secondParagraph).toBeInTheDocument();
+});
+
+test('se a pagina contem a imagem da pokedex', () => {
+  render(<About />);
+  const urlImg = `https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png`;
+  const getImg = screen.getByRole('img');
+
+  expect(getImg).toHaveAttribute('src', urlImg);
+});
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import { Router } from 'react-router-dom';
+// import { createMemoryHistory } from 'history';
+// import userEvent from '@testing-library/user-event';
+// import App from '../App';
+
+// test('Se aparece "No favorite pokemon found" quando nao tiver pokemon', () => {
+//   const customHistory = createMemoryHistory();
+//   render(
+//     <Router history={ customHistory }>
+//       <App />
+//     </Router>,
+//   );
+
+//   const linkMoreDetails = screen.getByRole('link', { name: 'More details' });
+
+//   userEvent.click(linkMoreDetails);
+
+//   const notFound = screen.getByText('No favorite pokemon found');
+//   expect(notFound).toBeInTheDocument();
+// });
