4aba9d88793cb3817a6729582f608f5cd12cd845
AntonioJSCardoso <antonio.js.cardoso26@gmail.com>
2021-10-24 21:58:02 -0300

    adicionando lint

---
+++ src/tests/utils/renderWithRouter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
+
+function renderWithRouter(component) {
+  const customHistory = createMemoryHistory();
+
+  return {
+    ...render(
+      <Router history={ customHistory }>
+        { component }
+      </Router>,
+    ),
+    history: customHistory,
+  };
+}
+
+export default renderWithRouter;
