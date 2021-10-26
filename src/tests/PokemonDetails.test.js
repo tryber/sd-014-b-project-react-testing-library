6aa1c131ef55a3164392c38e7a98261a4a9e6243
dineinunes <nunesvieiraclaudinei@outlook.com>
2021-10-26 17:38:30 -0300

    Lint

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,9 +1,3 @@
-import React from 'react';
-import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { PokemonDetails } from '../components';
-import renderWithRouter from '../renderWithRouter';
-
 describe('Tests the PokemonDetails.js application', () => {
   test('', () => {});
 });
