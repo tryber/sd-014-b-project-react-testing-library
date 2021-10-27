8609e3aed66e6ff8fb4b668fd3a175b1ec2d466f
Eduardomak <eduardomak92@gmail.com>
2021-10-27 20:24:52 -0300

    Requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../components/About';
+
+describe('teste do componente About', () => {
+  it('contém um text h2 About Pokédex', () => {
+    renderWithRouter(<About />);
+    const heading2 = screen.getByRole('heading', { name: /About Pokédex/i, level: 2 });
+    expect(heading2).toBeInTheDocument();
+  });
+  it('contém dois parágrafos', () => {
+    renderWithRouter(<About />);
+    const par1 = screen.getByText(/encyclopedia/i);
+    expect(par1).toBeInTheDocument();
+    const par2 = screen.getByText(/filter/i);
+    expect(par2).toBeInTheDocument();
+  });
+  it('a imagem certa é renderizada', () => {
+    renderWithRouter(<About />);
+    const img = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const image = screen.getByRole('img');
+    expect(image).toHaveAttribute('src', img);
+  });
+});
