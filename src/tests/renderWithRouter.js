71cbb074aa84a5c53c1b21b9f792b96aa075bf99
AntonioJSCardoso <antonio.js.cardoso26@gmail.com>
2021-10-26 23:51:08 -0300

    mais uma

--- src/tests/renderWithRouter.js
+++ src/tests/renderWithRouter.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
+import { Router } from 'react-router';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
 
-export default function renderWithRouter(component) {
-  const history = createMemoryHistory();
-  return ({
-    ...render(<Router history={ history }>{ component }</Router>), history,
-  });
+function renderWithRouter(component) {
+  const customHistory = createMemoryHistory(); 
+  return {
+    ...render(
+      <Router history={ customHistory }>
+        { component }
+      </Router>,
+    ), 
+    history: customHistory, 
+  };
 }
 
 export default renderWithRouter;
