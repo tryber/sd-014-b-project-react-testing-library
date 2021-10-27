cc239e20ea9012a2a7bba5737dfbd2ee8d759256
Diego Costa <dgrcosta@gmail.com>
2021-10-27 17:26:21 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+
+import renderWithRouter from './utils/renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const notFound = screen.getByText(/no favorite pokemon found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+
+  test('Verifica se é exibido os pokémons favoritos', () => {
+    const pokemons = [
+      {
+        averageWeight: {},
+        id: 1,
+        image: '',
+        name: 'Bulbasaur',
+        type: '',
+      }];
+
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+
+    const favoritePokemon = screen.getByRole('link', { name: 'More details' });
+    expect(favoritePokemon.href).toContain('/pokemons/1');
+  });
+});
