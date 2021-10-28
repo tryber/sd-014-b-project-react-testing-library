e4f80ae9a88c3158a559d5dc449ed5cdb1f6d58d
Eicke Yumoto <eickeyumoto@gmail.com>
2021-10-28 00:43:40 -0300

    requisito NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../components/renderWithRouter';
+import { NotFound } from '../components';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it(
+    'Teste se página contém um heading h2 com o texto Page requested not found 😭',
+    () => {
+      renderWithRouter(<NotFound />);
+
+      const notFound = screen.getByRole('heading', { level: 2 });
+      expect(notFound).toHaveTextContent('Page requested not found');
+    },
+  );
+
+  it('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+
+    const image = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+    expect(image.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
