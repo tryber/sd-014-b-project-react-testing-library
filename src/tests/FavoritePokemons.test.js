2a1b6f6a63c5faaa058f9f01a81dde9b012b4219
Eduardomak <eduardomak92@gmail.com>
2021-10-28 02:14:31 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../components';
+
+describe('testes do componente FavoritePokemons', () => {
+  it('a mensagem No favorite pokemon found é exibida se não houverem favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const text = screen.getByText(/No favorite pokemon found/i);
+    expect(text).toBeInTheDocument();
+  });
+  it('exibe os pokemons favoritos', () => {
+    const pokemon = [
+      {
+        id: 25,
+        name: 'Pikachu',
+        type: 'Electric',
+        averageWeight: {
+          value: '6.0',
+          measurementUnit: 'kg',
+        },
+        image: 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png',
+      },
+    ];
+    renderWithRouter(<FavoritePokemons pokemons={ pokemon } />);
+    const pokemonName = screen.getByText('Pikachu');
+    expect(pokemonName).toBeInTheDocument();
+  });
+});
