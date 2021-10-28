65ae1b09d8798b495d5cba0e6b68efb07d5a213e
Alexandre Fortes <fortes04alexandre@gmail.com>
2021-10-28 03:25:34 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Teste do componente <NotFound.js />', () => {
+  test('Verifica heading e imagem', () => {
+    render(<NotFound />);
+    const heading = screen.getByText('Page requested not found');
+    expect(heading).toBeInTheDocument();
+
+    const image = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
