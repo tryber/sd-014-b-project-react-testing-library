b20de096924c7abff70d210621136ee035526bf5
Theo Lima <theo.suporte@hotmail.com>
2021-10-28 00:24:44 -0300

    Implementa testes de About.js

--- src/tests/renderWithRouter.js
+++ src/tests/renderWithRouter.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { render } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
-import { render } from '@testing-library/react';
 
-const renderWithRouter = (component) => {
-  const history = createMemoryHistory();
-  return ({
-    ...render(<Router history={ history }>{component}</Router>), history,
-  });
-};
+function renderWithRouter(component) {
+  const customHistory = createMemoryHistory();
+  const selectorsRTL = render(
+    <Router history={ customHistory }>
+      { component }
+    </Router>,
+  );
+
+  return {
+    ...selectorsRTL,
+    history: customHistory,
+  };
+}
 
 export default renderWithRouter;
