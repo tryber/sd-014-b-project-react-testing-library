33a69e5ddbc2492de7d28a5ef5169336f66c4aef
Rogerio Maia Jr. - r0ger <rogeriomaiajr13@gmail.com>
2022-03-20 11:48:25 -0300

    Top NavLinks in App screen tests done

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,19 @@
-test('first commit :)', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+
+import App from '../App';
+
+describe('App.js tests', () => {
+  it('Top NavLinks in App screen', () => {
+    renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    const favPksLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    expect(homeLink).toBeDefined();
+    expect(aboutLink).toBeDefined();
+    expect(favPksLink).toBeDefined();
+  });
+});
